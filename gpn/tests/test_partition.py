ab7ba0b3439656f6a770e54a42402b45123b8988
Shawn Brown <shawnbrown@users.noreply.github.com>
2014-07-17 23:41:07 -0400

    Add test for planned insert_cells method.

--- gpn/tests/test_partition.py
+++ gpn/tests/test_partition.py
@@ -2,6 +2,11 @@
 import os
 import sqlite3
 import sys
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO  # New stdlib location in 3.0
+
 
 from gpn.tests import _unittest as unittest
 from gpn.tests.common import MkdtempTestCase
@@ -13,7 +18,7 @@ from gpn import TEMP_FILE
 from gpn import READ_ONLY
 
 
-class TestPartition(MkdtempTestCase):
+class TestInstantiation(MkdtempTestCase):
     def _make_partition(self, filename):
         global _create_partition
         self._existing_partition = filename
@@ -44,7 +49,6 @@ class TestPartition(MkdtempTestCase):
                                      sqlite3.IntegrityError), regex):
             cursor.execute('INSERT INTO cell DEFAULT VALUES')
 
-
     def test_new_partition(self):
         """Named Partitions that do not exist should be created."""
         filename = 'new_partition'
@@ -65,6 +69,73 @@ class TestPartition(MkdtempTestCase):
         ptn = Partition(mode=TEMP_FILE)
         self.assertIsNotNone(ptn._connect._temp_path)
         self.assertIsNone(ptn._connect._memory_conn)
+
+
+class TestInsert(unittest.TestCase):
+    @unittest.skip('Not yet implemented.')
+    def test_insert_cells(self):
+        self.maxDiff = None
+
+        fh = StringIO('state,county,town\n'
+                      'OH,Allen,Lima\n'
+                      'OH,Cuyahoga,Cleveland\n'
+                      'OH,Franklin,Columbus\n'
+                      'OH,Hamilton,Cincinnati\n'
+                      'OH,Montgomery,Dayton\n')
+        partition = Partition(mode=IN_MEMORY)
+        partition.insert_cells(fh)  # <- Inserting here!
+
+        connection = partition._connect()
+        cursor = connection.cursor()
+
+        # Hierarchy table.
+        cursor.execute('SELECT * FROM hierarchy ORDER BY hierarchy_level')
+        expected = [(1, 'state', 0), (2, 'county', 1), (3, 'town', 2)]
+        self.assertEqual(expected, cursor.fetchall())
+
+        # Cell table.
+        cursor.execute('SELECT * FROM cell ORDER BY cell_id')
+        expected = [(1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0)]
+        self.assertEqual(expected, cursor.fetchall())
+
+        # Label table.
+        cursor.execute('SELECT * FROM label ORDER BY label_id')
+        expected = [(1,  1, 'UNMAPPED'),   (2,  2, 'UNMAPPED'),
+                    (3,  3, 'UNMAPPED'),   (4,  1, 'OH'),
+                    (5,  2, 'Allen'),      (6,  3, 'Lima'),
+                    (7,  2, 'Cuyahoga'),   (8,  3, 'Cleveland'),
+                    (9,  2, 'Franklin'),   (10, 3, 'Columbus'),
+                    (11, 2, 'Hamilton'),   (12, 3, 'Cincinnati'),
+                    (13, 2, 'Montgomery'), (14, 3, 'Dayton')]
+        self.assertEqual(expected, cursor.fetchall())
+
+        # Cell_label table,
+        cursor.execute('SELECT * FROM cell_label ORDER BY cell_label_id')
+        expected = [(1,  1, 1, 1), (2,  1, 2, 2),  (3,  1, 3, 3),
+                    (4,  2, 1, 4), (5,  2, 2, 5),  (6,  2, 3, 6),
+                    (8,  3, 1, 4), (8,  3, 2, 7),  (9,  3, 3, 8),
+                    (10, 4, 1, 4), (11, 4, 2, 9),  (12, 4, 3, 10),
+                    (13, 5, 1, 4), (14, 5, 2, 11), (15, 5, 3, 12),
+                    (16, 6, 1, 4), (17, 6, 2, 13), (18, 6, 3, 14)]
+        self.assertEqual(expected, cursor.fetchall())
+
+        # Partition table.
+        cursor.execute('SELECT partition_id, partition_hash '
+                       'FROM partition ORDER BY partition_id')
+        self.assertEqual([], cursor.fetchall())
+
+        # Other tables should be empty.
+        cursor.execute('SELECT * FROM edge')
+        self.assertEqual([], cursor.fetchall())
+
+        cursor.execute('SELECT * FROM weight')
+        self.assertEqual([], cursor.fetchall())
+
+        cursor.execute('SELECT * FROM relation')
+        self.assertEqual([], cursor.fetchall())
+
+        cursor.execute('SELECT * FROM relation_weight')
+        self.assertEqual([], cursor.fetchall())
 
 
 if __name__ == '__main__':
