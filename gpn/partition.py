e8c431a5173e73cb0e583d0cbb0bc3fab24070e9
Shawn Brown <shawnbrown@users.noreply.github.com>
2014-07-25 00:26:19 -0400

    Remove unnecessary join to hierarchy table.

--- gpn/partition.py
+++ gpn/partition.py
@@ -201,7 +201,6 @@ class Partition(object):
             SELECT cell_id, hierarchy_id, label_value
             FROM cell_label
             NATURAL JOIN label
-            NATURAL JOIN hierarchy
             ORDER BY cell_id, hierarchy_id, label_value
         """)
         sha256 = hashlib.sha256()
