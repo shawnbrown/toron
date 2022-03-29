047b9dd60e4c0b61503ce30ea5226314e788b391
Shawn Brown <shawnbrown@users.noreply.github.com>
2022-03-29 01:27:07 -0400

    Rename package from "gpn" to "toron".
    Most of my packages have had very plain, utilitarian names but
    I thought it would be nice to use a more metaphorical name for
    this project.
    
    A key feature of this tool kit is to reconcile non-equi-joinable
    datasets that have arbitrary levels of granularity. A topological
    soliton (or toron) serves an analogous function:
    
        A topological soliton or "toron" occurs when two adjoining
        structures or spaces are in some way "out of phase" with each
        other in ways that make a seamless transition between them
        impossible.
    
        <https://en.wikipedia.org/wiki/Topological_soliton>

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='toron',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/shawnbrown/toron',
+    author='Shawn Brown',
+    author_email='shawnbrown@users.noreply.github.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
+
