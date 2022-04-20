45d318823f8b5d76385bd3ab9a9608906484106b
Shawn Brown <shawnbrown@users.noreply.github.com>
2022-04-20 03:10:55 -0400

    Move node schema info into _node_schema module.

--- toron/_node_schema.py
+++ toron/_node_schema.py
@@ -1,13 +1,48 @@
-"""Simplified, Python-native object serialization using strings.
+"""Database schema functions and information for Toron node files.
 
-Differences from JSON:
+Toron nodes are stored as individual files. The file format is
+managed, internally, as a relational database. The schema for this
+database is shown below as a simplified ERD (entity relationship
+diagram). SQL foreign key relationships are represented with hyphen
+and pipe characters ('-' and '|'). Other, more complex relationships
+are represented with bullet points ('•') and these are enforced at
+the application layer:
 
-* Supports ``tuple`` and ``set`` object (JSON does not support these types).
-* Restricts serialization to instances of exact type matches to prevent
-  data loss. E.g., OrderedDict would raise an exception because they cannot
-  be serialized using literal object notation.
-* Collections cannot be nested--containers can only contain non-container
-  types (JSON allows nested containers).
+                                 +------------------+
+  +---------------------+        | relation         |
+  | edge                |        +------------------+
+  +---------------------+        | relation_id      |     •••• <Other Node>
+  | edge_id             |------->| edge_id          |     •
+  | name                |  ••••••| other_element_id |<•••••
+  | description         |  •  •••| element_id       |<-+     +-------------+
+  | type_info           |  •  •  | proportion       |  |     | quantity    |
+  | optional_attributes |  •  •  | mapping_level    |  |     +-------------+
+  | other_uuid          |  •  •  +------------------+  |     | quantity_id |
+  | other_filename_hint |  •  •                        |  +->| location_id |
+  | other_element_hash  |<••  •                        |  |  | attributes  |
+  | is_complete         |<•••••       +----------------+  |  | value       |
+  +---------------------+             |                   |  +-------------+
+                                      |                   |
+                      +------------+  |  +-------------+  |  +--------------+
+                      | element    |  |  | location    |  |  | structure    |
+                      +------------+  |  +-------------+  |  +--------------+
+              +-------| element_id |--+  | location_id |--+  | structure_id |
+              |       | label_a    |••••>| label_a     |<••••| label_a      |
+              |       | label_b    |••••>| label_b     |<••••| label_b      |
+              |       | label_c    |••••>| label_c     |<••••| label_c      |
+              |       | ...        |••••>| ...         |<••••| ...          |
+              |       +------------+     +-------------+     +--------------+
+              |
+              |  +----------------+                            +----------+
+              |  | weight         |     +----------------+     | property |
+              |  +----------------+     | weight_info    |     +----------+
+              |  | weight_id      |     +----------------+     | key      |
+              |  | weight_info_id |<----| weight_info_id |     | value    |
+              +->| element_id     |•••  | name           |     +----------+
+                 | value          |  •  | description    |
+                 +----------------+  •  | type_info      |
+                                     ••>| is_complete    |
+                                        +----------------+
 """
 
 from ast import literal_eval
@@ -126,4 +161,80 @@ def loads(s, errors='strict'):
 
         msg = "*errors* must be 'strict', 'warn', or 'ignore'"
         raise ValueError(msg)
+
+
+_schema_script = """
+    PRAGMA foreign_keys = ON;
+
+    CREATE TABLE edge(
+        edge_id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        description TEXT,
+        type_info PYTEXT_DICT NOT NULL,
+        optional_attributes PYTEXT_DICT,
+        other_uuid TEXT CHECK (other_uuid LIKE '________-____-____-____-____________') NOT NULL,
+        other_filename_hint TEXT NOT NULL,
+        other_element_hash TEXT,
+        is_complete INTEGER CHECK (is_complete IN (0, 1)),
+        UNIQUE (name, other_uuid)
+    );
+
+    CREATE TABLE relation(
+        relation_id INTEGER PRIMARY KEY,
+        edge_id INTEGER,
+        other_element_id INTEGER NOT NULL,
+        element_id INTEGER,
+        proportion REAL CHECK (0.0 < proportion AND proportion <= 1.0) NOT NULL,
+        mapping_level INTEGER NOT NULL,
+        FOREIGN KEY(edge_id) REFERENCES edge(edge_id),
+        FOREIGN KEY(element_id) REFERENCES element(element_id),
+        UNIQUE (edge_id, other_element_id, element_id)
+    );
+
+    CREATE TABLE element(
+        element_id INTEGER PRIMARY KEY AUTOINCREMENT  /* <- Must not reuse id values. */
+        /* label columns added programmatically */
+    );
+
+    CREATE TABLE location(
+        location_id INTEGER PRIMARY KEY
+        /* label columns added programmatically */
+    );
+
+    CREATE TABLE structure(
+        structure_id INTEGER PRIMARY KEY
+        /* label columns added programmatically */
+    );
+
+    CREATE TABLE quantity(
+        quantity_id INTEGER PRIMARY KEY,
+        location_id INTEGER,
+        attributes PYTEXT_DICT NOT NULL,
+        value NUMERIC NOT NULL,
+        FOREIGN KEY(location_id) REFERENCES location(location_id)
+    );
+
+    CREATE TABLE weight_info(
+        weight_info_id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        description TEXT,
+        type_info PYTEXT_DICT NOT NULL,
+        is_complete INTEGER CHECK (is_complete IN (0, 1)),
+        UNIQUE (name)
+    );
+
+    CREATE TABLE weight(
+        weight_id INTEGER PRIMARY KEY,
+        weight_info_id INTEGER,
+        element_id INTEGER,
+        value REAL NOT NULL,
+        FOREIGN KEY(element_id) REFERENCES element(element_id),
+        FOREIGN KEY(weight_info_id) REFERENCES weight_info(weight_info_id)
+    );
+
+    CREATE TABLE property(
+        key TEXT PRIMARY KEY NOT NULL,
+        value PYTEXT_LITERAL
+    );
+"""
 
