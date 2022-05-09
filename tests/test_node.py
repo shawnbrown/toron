122578715c73642eb96fc04b88497b6ff783849c
Shawn Brown <shawnbrown@users.noreply.github.com>
2022-05-09 01:26:54 -0400

    Add mode to Node.__init__() and add path property.

--- tests/test_node.py
+++ tests/test_node.py
@@ -1,9 +1,15 @@
 """Tests for toron/node.py module."""
 
 import unittest
+
+from .common import TempDirTestCase
+
 from toron.node import Node
 
 
-class TestNode(unittest.TestCase):
-    pass
+class TestNode(TempDirTestCase):
+    def test_new_node(self):
+        node = Node('mynode.toron')
+        self.assertEqual(node.path, 'mynode.toron')
+        self.assertEqual(node.mode, 'rwc')
 
--- toron/node.py
+++ toron/node.py
@@ -1,12 +1,18 @@
 """Node implementation for the Toron project."""
 
+import os
+
 from ._node_schema import connect
 
 
 class Node(object):
-    def __init__(self, path):
+    def __init__(self, path, mode='rwc'):
         path = os.fspath(path)
+        connect(path, mode=mode).close()  # Verify path to Toron node file.
         self._path = path
+        self.mode = mode
 
-        connect(path).close()  # Verify that path is a Toron node.
+    @property
+    def path(self):
+        return self._path
 
