c910539695fa5ccf7dd6b0876303305a3d883bb1
Shawn Brown <shawnbrown@users.noreply.github.com>
2022-05-15 20:16:07 -0400

    Change Node methods to use transaction() context manager.

--- toron/node.py
+++ toron/node.py
@@ -5,7 +5,7 @@ from contextlib import closing
 from itertools import compress
 
 from ._node_schema import connect
-from ._node_schema import savepoint
+from ._node_schema import transaction
 from ._node_schema import _get_column_names
 from ._node_schema import _make_sql_new_labels
 from ._node_schema import _make_sql_insert_elements
@@ -20,36 +20,33 @@ class Node(object):
         connect(path, mode=mode).close()  # Verify path to Toron node file.
         self._path = path
         self.mode = mode
+        self._transaction = lambda: transaction(self.path, mode=self.mode)
 
     @property
     def path(self):
         return self._path
 
     def add_columns(self, columns):
-        with closing(connect(self.path, mode=self.mode)) as con:
-            with closing(con.cursor()) as cur:
-                with savepoint(cur):
-                    for stmnt in _make_sql_new_labels(cur, columns):
-                        cur.execute(stmnt)
+        with self._transaction() as cur:
+            for stmnt in _make_sql_new_labels(cur, columns):
+                cur.execute(stmnt)
 
     def add_elements(self, iterable, columns=None):
         iterator = iter(iterable)
         if not columns:
             columns = next(iterator)
 
-        with closing(connect(self.path, mode=self.mode)) as con:
-            with closing(con.cursor()) as cur:
-                with savepoint(cur):
-                    # Get allowed columns and build selectors values.
-                    allowed_columns = _get_column_names(cur, 'element')
-                    selectors = tuple((col in allowed_columns) for col in columns)
+        with self._transaction() as cur:
+            # Get allowed columns and build selectors values.
+            allowed_columns = _get_column_names(cur, 'element')
+            selectors = tuple((col in allowed_columns) for col in columns)
 
-                    # Filter column names and iterator rows to allowed columns.
-                    columns = compress(columns, selectors)
-                    iterator = (tuple(compress(row, selectors)) for row in iterator)
+            # Filter column names and iterator rows to allowed columns.
+            columns = compress(columns, selectors)
+            iterator = (tuple(compress(row, selectors)) for row in iterator)
 
-                    sql = _make_sql_insert_elements(cur, columns)
-                    cur.executemany(sql, iterator)
+            sql = _make_sql_insert_elements(cur, columns)
+            cur.executemany(sql, iterator)
 
     def add_weights(self, iterable, columns=None, *, name, type_info, description=None):
         iterator = iter(iterable)
@@ -63,27 +60,25 @@ class Node(object):
             msg = f'Name {name!r} does not appear in columns: {columns_string}'
             raise ValueError(msg)
 
-        with closing(connect(self.path, mode=self.mode)) as con:
-            with closing(con.cursor()) as cur:
-                with savepoint(cur):
-                    weight_id = _insert_weight_get_id(cur, name, type_info, description)
+        with self._transaction() as cur:
+            weight_id = _insert_weight_get_id(cur, name, type_info, description)
 
-                    # Get allowed columns and build selectors values.
-                    allowed_columns = _get_column_names(cur, 'element')
-                    selectors = tuple((col in allowed_columns) for col in columns)
+            # Get allowed columns and build selectors values.
+            allowed_columns = _get_column_names(cur, 'element')
+            selectors = tuple((col in allowed_columns) for col in columns)
 
-                    # Filter column names and iterator rows to allowed columns.
-                    columns = compress(columns, selectors)
-                    def mkrow(row):
-                        weightid_and_value = (weight_id, row[weight_pos])
-                        element_labels = tuple(compress(row, selectors))
-                        return weightid_and_value + element_labels
-                    iterator = (mkrow(row) for row in iterator)
+            # Filter column names and iterator rows to allowed columns.
+            columns = compress(columns, selectors)
+            def mkrow(row):
+                weightid_and_value = (weight_id, row[weight_pos])
+                element_labels = tuple(compress(row, selectors))
+                return weightid_and_value + element_labels
+            iterator = (mkrow(row) for row in iterator)
 
-                    # Insert element_weight records.
-                    sql = _make_sql_insert_element_weight(cur, columns)
-                    cur.executemany(sql, iterator)
+            # Insert element_weight records.
+            sql = _make_sql_insert_element_weight(cur, columns)
+            cur.executemany(sql, iterator)
 
-                    # Update "weight.is_complete" value (set to 1 or 0).
-                    _update_weight_is_complete(cur, weight_id)
+            # Update "weight.is_complete" value (set to 1 or 0).
+            _update_weight_is_complete(cur, weight_id)
 
