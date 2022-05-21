ba13a558bc991dedcce81d767b097117182e062b
Shawn Brown <shawnbrown@users.noreply.github.com>
2022-05-21 17:07:38 -0400

    Reorder `transaction` and `savepoint` definitions.
    Since `savepoint` is used by `transaction`, `savepoint` should be
    defined first.

--- toron/_node_schema.py
+++ toron/_node_schema.py
@@ -429,29 +429,6 @@ def connect(path, mode='rwc'):
     return con
 
 
-@contextmanager
-def transaction(path_or_connection, mode=None):
-    """A context manager that yields a cursor that runs in an
-    isolated transaction. If the context manager exits without
-    errors, the transaction is committed. If an exception is
-    raised, all changes are rolled-back.
-    """
-    if isinstance(path_or_connection, sqlite3.Connection):
-        connection = path_or_connection
-        connection_close = lambda: None  # Don't close already-existing cursor.
-    else:
-        connection = connect(path_or_connection, mode=mode)
-        connection_close = connection.close
-
-    cursor = connection.cursor()
-    try:
-        with savepoint(cursor):
-            yield cursor
-    finally:
-        cursor.close()
-        connection_close()
-
-
 _SAVEPOINT_NAME_GENERATOR = (f'svpnt{n}' for n in itertools.count())
 
 
@@ -488,6 +465,29 @@ class savepoint(object):
             self.cursor.execute(f'RELEASE {self.name}')
         else:
             self.cursor.execute(f'ROLLBACK TO {self.name}')
+
+
+@contextmanager
+def transaction(path_or_connection, mode=None):
+    """A context manager that yields a cursor that runs in an
+    isolated transaction. If the context manager exits without
+    errors, the transaction is committed. If an exception is
+    raised, all changes are rolled-back.
+    """
+    if isinstance(path_or_connection, sqlite3.Connection):
+        connection = path_or_connection
+        connection_close = lambda: None  # Don't close already-existing cursor.
+    else:
+        connection = connect(path_or_connection, mode=mode)
+        connection_close = connection.close
+
+    cursor = connection.cursor()
+    try:
+        with savepoint(cursor):
+            yield cursor
+    finally:
+        cursor.close()
+        connection_close()
 
 
 class DataAccessLayer(object):
