44ba40903612fe0fa743f691e6ec5cd458e9885e
Shawn Brown <shawnbrown@users.noreply.github.com>
2022-07-25 00:44:30 -0400

    Add `install_requires` and `python_requires` args to setup.py.

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ except ImportError:
 
 
 setup(
+    # Required meta-data:
     name='toron',
     version='0.0.1',
+    url='https://github.com/shawnbrown/toron',
+    packages=['toron'],
+    # Additional fields:
+    install_requires=['typing_extensions;python_version<"3.10"'],
+    python_requires='>=3.7',
     description='',
     long_description='',
-    url='https://github.com/shawnbrown/toron',
     author='Shawn Brown',
     author_email='shawnbrown@users.noreply.github.com',
-    packages=[],
     classifiers=['Development Status :: 1 - Planning'],
 )
 
