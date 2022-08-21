ea73976f28d8f644e0a68d9763b02b52e71357a7
Shawn Brown <shawnbrown@users.noreply.github.com>
2022-08-20 21:32:24 -0400

    Add argument checks to Selector to require needed values.

--- tests/test_selectors.py
+++ tests/test_selectors.py
@@ -6,6 +6,20 @@ from toron._selectors import Selector
 
 
 class TestSelector(unittest.TestCase):
-    def test_init(self):
-        selector = Selector('abc')
+    def test_instantiation(self):
+        Selector('abc')
+        Selector('abc', '=', 'xyz')
+        Selector('abc', '=', 'xyz', ignore_case=True)
+
+        with self.assertRaises(TypeError):
+            Selector()
+
+        with self.assertRaises(TypeError):
+            Selector('abc', '=', None)
+
+        with self.assertRaises(TypeError):
+            Selector('abc', None, 'xyz')
+
+        with self.assertRaises(TypeError):
+            Selector('abc', ignore_case=True)
 
--- toron/_selectors.py
+++ toron/_selectors.py
@@ -11,6 +11,13 @@ class Selector(object):
         val: Optional[str] = None,
         ignore_case: Optional[bool] = None,
     ) -> None:
+        """Initialize Selector instance."""
+        if (op and not val) or (val and not op):
+            raise TypeError('must provide `op` and `val` together')
+
+        if ignore_case and not (op and val):
+            raise TypeError('got `ignore_case`, must also provide `op` and `val`')
+
         self.attr = attr
         self.op = op
         self.val = val
