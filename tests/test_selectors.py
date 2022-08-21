4ba219cd904286c349aba04ba7ca73c97497e1c2
Shawn Brown <shawnbrown@users.noreply.github.com>
2022-08-20 22:55:53 -0400

    Simplify test values to make test cases easier to read.

--- tests/test_selectors.py
+++ tests/test_selectors.py
@@ -7,79 +7,80 @@ from toron._selectors import Selector
 
 class TestSelector(unittest.TestCase):
     def test_instantiation(self):
-        Selector('abc')
-        Selector('abc', '=', 'xyz')
-        Selector('abc', '=', 'xyz', ignore_case=True)
+        Selector('aaa')
+        Selector('aaa', '=', 'xxx')
+        Selector('aaa', '=', 'xxx', ignore_case=True)
 
         with self.assertRaises(TypeError):
             Selector()
 
         with self.assertRaises(TypeError):
-            Selector('abc', '=', None)
+            Selector('aaa', '=', None)
 
         with self.assertRaises(TypeError):
-            Selector('abc', None, 'xyz')
+            Selector('aaa', None, 'xxx')
 
         with self.assertRaises(TypeError):
-            Selector('abc', ignore_case=True)
+            Selector('aaa', ignore_case=True)
 
     def test_match_any_value(self):
-        selector = Selector('abc')
-        self.assertTrue(selector({'abc': 'xyz'}))
-        self.assertTrue(selector({'abc': 'qrs'}))
-        self.assertFalse(selector({'jkl': 'qrs'}))  # <- No attribute 'abc'.
-        self.assertFalse(selector({'abc': ''}))  # <- Value is not truthy.
+        selector = Selector('aaa')
+        self.assertTrue(selector({'aaa': 'xxx'}))
+        self.assertTrue(selector({'aaa': 'yyy'}))
+        self.assertFalse(selector({'BBB': 'yyy'}))  # <- No attribute 'aaa'.
+        self.assertFalse(selector({'aaa': ''}))  # <- Value is not truthy.
 
     def test_match_exact_value(self):
-        selector = Selector('abc', '=', 'xyz')
-        self.assertTrue(selector({'abc': 'xyz'}))
-        self.assertFalse(selector({'abc': 'qrs'}))  # <- Value does not match.
-        self.assertFalse(selector({'jkl': 'xyz'}))  # <- No attribute 'abc'.
-        self.assertFalse(selector({'abc': 'XYZ'}))  # <- Matching is case-sensitive.
+        selector = Selector('aaa', '=', 'xxx')
+        self.assertTrue(selector({'aaa': 'xxx'}))
+        self.assertFalse(selector({'aaa': 'YYY'}))  # <- Value does not match.
+        self.assertFalse(selector({'BBB': 'xxx'}))  # <- No attribute 'aaa'.
+        self.assertFalse(selector({'aaa': 'XXX'}))  # <- Matching is case-sensitive.
 
     def test_match_whitespace_separated_list(self):
-        selector = Selector('abc', '~=', 'xyz')
-        self.assertTrue(selector({'abc': 'ghi xyz qrs'}))
-        self.assertTrue(selector({'abc': 'xyz'}))  # <- Exact value should match, too.
-        self.assertFalse(selector({'abc': 'ghi-xyz-qrs'}))  # <- Not whitespace separated.
-        self.assertFalse(selector({'abc': 'ghi wxyz qrs'}))  # <- Substring won't match (must be exact).
+        selector = Selector('aaa', '~=', 'xxx')
+        self.assertTrue(selector({'aaa': 'ZZZ xxx YYY'}))
+        self.assertTrue(selector({'aaa': 'xxx'}))  # <- Exact value should match, too.
+        self.assertFalse(selector({'aaa': 'ZZZ-xxx-YYY'}))  # <- Not whitespace separated.
+        self.assertFalse(selector({'aaa': 'ZZZ wxxx YYY'}))  # <- Substring won't match (must be exact).
 
         # Check irregular whitespace.
-        self.assertTrue(selector({'abc': 'ijk\tlmn\fopq\r\nxyz\nrst   uvw'}))
+        self.assertTrue(selector({'aaa': 'UUU\tVVV\fWWW\r\nxxx\nYYY   ZZZ'}))
 
     def test_match_starts_with_value_and_hyphen(self):
-        selector = Selector('abc', '|=', 'xyz')
-        self.assertTrue(selector({'abc': 'xyz-pqr'}))
-        self.assertTrue(selector({'abc': 'xyz'}))  # <- Exact value should match, too.
-        self.assertFalse(selector({'abc': 'xyz pqr'}))  # <- Cannot be followed by any char except "-".
-        self.assertFalse(selector({'abc': 'ghi-xyz-pqr'}))  # <- Does not start with "xyz".
+        selector = Selector('aaa', '|=', 'xxx')
+        self.assertTrue(selector({'aaa': 'xxx-YYY'}))
+        self.assertTrue(selector({'aaa': 'xxx'}))  # <- Exact value should match, too.
+        self.assertFalse(selector({'aaa': 'xxx YYY'}))  # <- Cannot be followed by any char except "-".
+        self.assertFalse(selector({'aaa': 'ZZZ-xxx-YYY'}))  # <- Does not start with "xxx".
 
     def test_match_starts_with_value(self):
-        selector = Selector('abc', '^=', 'xyz')
-        self.assertTrue(selector({'abc': 'xyzpqr'}))
-        self.assertTrue(selector({'abc': 'xyz-pqr'}))
-        self.assertTrue(selector({'abc': 'xyz pqr'}))
-        self.assertTrue(selector({'abc': 'xyz'}))  # <- Exact value should match, too.
-        self.assertFalse(selector({'abc': 'wxyz'}))  # <- Does not start with "xyz".
+        selector = Selector('aaa', '^=', 'xxx')
+        self.assertTrue(selector({'aaa': 'xxxYYY'}))
+        self.assertTrue(selector({'aaa': 'xxx-YYY'}))
+        self.assertTrue(selector({'aaa': 'xxx YYY'}))
+        self.assertTrue(selector({'aaa': 'xxx'}))  # <- Exact value should match, too.
+        self.assertFalse(selector({'aaa': 'Zxxx'}))  # <- Does not start with "xxx".
 
     def test_match_ends_with_value(self):
-        selector = Selector('abc', '$=', 'xyz')
-        self.assertTrue(selector({'abc': 'pqrxyz'}))
-        self.assertTrue(selector({'abc': 'pqr-xyz'}))
-        self.assertTrue(selector({'abc': 'pqr xyz'}))
-        self.assertTrue(selector({'abc': 'xyz'}))  # <- Exact value should match, too.
-        self.assertFalse(selector({'abc': 'xyz0'}))  # <- Does not end with "xyz".
+        selector = Selector('aaa', '$=', 'xxx')
+        self.assertTrue(selector({'aaa': 'ZZZxxx'}))
+        self.assertTrue(selector({'aaa': 'ZZZ-xxx'}))
+        self.assertTrue(selector({'aaa': 'ZZZ xxx'}))
+        self.assertTrue(selector({'aaa': 'xxx'}))  # <- Exact value should match, too.
+        self.assertFalse(selector({'aaa': 'xxxZ'}))  # <- Does not end with "xxx".
 
     def test_match_substring_value(self):
-        selector = Selector('abc', '*=', 'xyz')
-        self.assertTrue(selector({'abc': 'PQRxyzGHI'}))
-        self.assertTrue(selector({'abc': 'PQR-xyz-GHI'}))
-        self.assertTrue(selector({'abc': 'PQR xyz GHI'}))
-        self.assertTrue(selector({'abc': 'xyz'}))  # <- Exact value should match, too.
-        self.assertFalse(selector({'abc': 'PQRx-yz-GHI'}))  # <- No matching substring.
+        selector = Selector('aaa', '*=', 'xxx')
+        self.assertTrue(selector({'aaa': 'ZZZxxxYYY'}))
+        self.assertTrue(selector({'aaa': 'ZZZ-xxx-YYY'}))
+        self.assertTrue(selector({'aaa': 'xxx'}))  # <- Exact value should match, too.
+        self.assertTrue(selector({'aaa': 'ZZZ xxx YYY'}))
+        self.assertFalse(selector({'aaa': 'ZZZ XXX YYY'}))  # <- Matching is case-sensitive.
+        self.assertFalse(selector({'aaa': 'ZZZx-xx-YYY'}))  # <- No matching substring.
 
     def test_unknown_operator(self):
         regex = r"unknown operator: '//"
         with self.assertRaisesRegex(ValueError, regex):
-            Selector('abc', '//', 'xyz')
+            Selector('aaa', '//', 'xxx')
 
