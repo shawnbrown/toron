4221981e1a67f66c4f110e0fa1a9cbe23cefe864
Shawn Brown <shawnbrown@users.noreply.github.com>
2023-01-21 13:29:39 -0500

    Clean up test_graph.py stub code.

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -8,20 +8,7 @@ from toron.graph import add_edge
 
 class TestAddEdge(unittest.TestCase):
     def setUp(self):
-        self.node1 = Node()
-        self.node2 = Node()
-        #self.dal = self.node._dal
-        #self.cursor = self.dal._get_connection().cursor()
+        pass
 
     def test_add_edge(self):
         pass
-        #self.node.add_index_columns(['A', 'B', 'C'])
-        #
-        #columns = get_column_names(self.cursor, 'label_index')
-        #self.assertEqual(columns, ['index_id', 'A', 'B', 'C'])
-        #
-        #self.cursor.execute('SELECT * FROM main.structure')
-        #actual = {row[1:] for row in self.cursor.fetchall()}
-        #expected = {(0, 0, 0), (1, 1, 1)}  # The trivial topology.
-        #self.assertEqual(actual, expected)
-
