257c89a5cd9d410b4d3480759844c107b1a5ba5e
Shawn Brown <shawnbrown@users.noreply.github.com>
2023-05-21 17:50:29 -0400

    Add Mapper._find_matches_format_data() and initial tests.

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+from toron.node import Node
 from toron._mapper import (
     Mapper,
 )
@@ -45,3 +46,57 @@ class TestMapper(unittest.TestCase):
             (2, '["B"]', '["B", "y"]', 80.0),
         }
         self.assertEqual(set(mapper.cur.fetchall()), expected)
+
+
+class TestMapperFindMatches(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.data = [
+            ['idx', 'population', 'idx1', 'idx2'],
+            ['A', 10, 'A', 'x'],
+            ['A', 70, 'A', 'y'],
+            ['B', 20, 'B', 'x'],
+            ['B', 60, 'B', 'y'],
+            ['C', 30, 'C', 'x'],
+            ['C', 50, 'C', 'y'],
+        ]
+
+    def test_find_matches_format_data_exact(self):
+        mapper = Mapper(self.data, 'population')
+
+        node = Node()
+        node_data = [
+            ['idx1', 'idx2', 'wght'],
+            ['A', 'x', 3],
+            ['A', 'y', 15],
+            ['B', 'x', 3],
+            ['B', 'y', 7],
+            ['C', 'x', 13],
+            ['C', 'y', 22],
+        ]
+        node.add_index_columns(['idx1', 'idx2'])
+        node.add_index_records(node_data)
+        node.add_weights(node_data, 'wght', selectors=['[attr1]'])
+
+        keys = ['idx1', 'idx2']
+        iterable = [
+            # Records contain `(label_values, run_id)`.
+            ('["A", "x"]', 101),
+            ('["A", "y"]', 102),
+            ('["B", "x"]', 103),
+            ('["B", "y"]', 104),
+            ('["C", "x"]', 105),
+            ('["C", "y"]', 106),
+        ]
+        formatted = mapper._find_matches_format_data(node, keys, iterable)
+        result = [(a, b, list(c)) for a, b, c in formatted]
+
+        expected = [
+            ([101], {'idx1': 'A', 'idx2': 'x'}, [(1, 'A', 'x')]),
+            ([102], {'idx1': 'A', 'idx2': 'y'}, [(2, 'A', 'y')]),
+            ([103], {'idx1': 'B', 'idx2': 'x'}, [(3, 'B', 'x')]),
+            ([104], {'idx1': 'B', 'idx2': 'y'}, [(4, 'B', 'y')]),
+            ([105], {'idx1': 'C', 'idx2': 'x'}, [(5, 'C', 'x')]),
+            ([106], {'idx1': 'C', 'idx2': 'y'}, [(6, 'C', 'y')]),
+        ]
+        self.assertEqual(result, expected)
--- toron/_mapper.py
+++ toron/_mapper.py
@@ -1,8 +1,21 @@
 """Tools for building correspondence mappings between label sets."""
 
 import sqlite3
+from itertools import (
+    groupby,
+)
 from json import (
     dumps as _dumps,
+    loads as _loads,
+)
+from ._typing import (
+    Dict,
+    Iterable,
+    Iterator,
+    List,
+    Sequence,
+    Tuple,
+    TYPE_CHECKING,
 )
 
 from ._utils import (
@@ -10,6 +23,9 @@ from ._utils import (
     make_readerlike,
     parse_edge_shorthand,
 )
+
+if TYPE_CHECKING:
+    from .node import Node
 
 
 class Mapper(object):
@@ -77,3 +93,63 @@ class Mapper(object):
             weight = row[weight_pos]
             sql = 'INSERT INTO temp.source_mapping VALUES (NULL, ?, ?, ?)'
             self.cur.execute(sql, (left_labels, right_labels, weight))
+
+    @staticmethod
+    def _find_matches_format_data(
+        node: 'Node', column_names: Sequence[str], iterable: Iterable[Tuple[str, int]]
+    ) -> Iterator[Tuple[List[int], Dict[str, str], Iterator[Tuple]]]:
+        """Takes a *node*, a sequence of label *keys*, and an *iterable*
+        containing ``(label_values, run_id)`` records. Returns an
+        iterator of ``(run_ids, where_dict, matches)`` records.
+
+        .. code-block::
+
+            >>> node = Node(...)
+            >>> column_names = ['col1', 'col2']
+            >>> iterable = [
+            ...     ('["A", "x"]', 101),
+            ...     ('["A", "y"]', 102),
+            ...     ('["B", "x"]', 103),
+            ...     ('["B", "y"]', 104),
+            ...     ('["C", "x"]', 105),
+            ...     ('["C", "y"]', 106),
+            ... ]
+            >>> formatted = dal._find_matches_format_data(node, column_names, iterable)
+            >>> for run_ids, where_dict, matches in formatted:
+            ...     print(f'{run_ids=}  {where_dict=}  {list(matches)=}')
+            ...
+            run_ids=[101]  where_dict={'col1': 'A', 'col2': 'x'}  list(matches)=[(1, 'A', 'x')]
+            run_ids=[102]  where_dict={'col1': 'A', 'col2': 'y'}  list(matches)=[(2, 'A', 'y')]
+            run_ids=[103]  where_dict={'col1': 'B', 'col2': 'x'}  list(matches)=[(3, 'B', 'x')]
+            run_ids=[104]  where_dict={'col1': 'B', 'col2': 'y'}  list(matches)=[(4, 'B', 'y')]
+            run_ids=[105]  where_dict={'col1': 'C', 'col2': 'x'}  list(matches)=[(5, 'C', 'x')]
+            run_ids=[106]  where_dict={'col1': 'C', 'col2': 'y'}  list(matches)=[(6, 'C', 'y')]
+        """
+        # Group rows using `label_values` as the key.
+        def get_label_values(row):
+            label_values, _ = row  # Discards `run_id` value.
+            return label_values
+
+        grouped = groupby(iterable, key=get_label_values)
+
+        # Helper function to format records as where_dicts.
+        def get_where_dict(x):
+            return dict((k, v) for k, v in zip(column_names, _loads(x)) if v)
+
+        # Helper function to format groups as lists of `run_id` values.
+        def get_run_ids(group):
+            return [run_id for _, run_id in group]  # Discards `label_values` key.
+
+        items = ((get_where_dict(k), get_run_ids(g)) for k, g in grouped)
+
+        # Unzip items into separate where_dict and run_id containers.
+        where_dicts, grouped_run_ids = zip(*items)
+
+        # Get node matches (NOTE: accessing internal ``_dal`` directly).
+        grouped_matches = node._dal.index_records_grouped(where_dicts)
+
+        # Reformat records for output.
+        zipped = zip(grouped_run_ids, grouped_matches)
+        run_ids_where_dict_matches = ((x, y, z) for (x, (y, z)) in zipped)
+
+        return run_ids_where_dict_matches
