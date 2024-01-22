ecbe362791e6a34de087a99bc6f85615c9870e2d
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-01-22 00:43:12 -0500

    Reorganize project from "flat layout" to "src layout".
    Update run-tests.py to add 'src' to PYTHONPATH.

--- run-tests.py
+++ run-tests.py
@@ -5,15 +5,18 @@ import sys
 
 
 args = [
-    sys.executable or 'python',  # Python interpreter to call for testing.
-    '-B',  # Don't write .pyc files on import.
-    '-O',  # Optimize: remove assert and __debug__-dependent statements.
-    '-W', 'default',  # Enable default handling for all warnings.
-    '-m', 'unittest', # Run the unittest module as a script.
-    'discover',  # Use test discovery.
-    '-s', 'tests',  # Start discovery in 'tests' directory.
+    sys.executable or 'python',       # Python interpreter to call for testing.
+    '-B',                             # Don't write .pyc files on import.
+    '-O',                             # Remove assert and __debug__-dependent statements.
+    '-W', 'default',                  # Enable default handling for all warnings.
+    '-m', 'unittest',                 # Run the unittest module as a script.
+    'discover',                       # Use test discovery.
+    '-s', 'tests',                    # Start discovery in 'tests' directory.
     '-t', os.path.dirname(__file__),  # Set top-level of project.
 ]
 args.extend(sys.argv[1:])  # Append any arguments passed to script (-v, etc.).
-sys.exit(subprocess.call(args))
 
+env = os.environ.copy()
+env['PYTHONPATH'] = 'src'  # Make package in 'src' importable.
+
+sys.exit(subprocess.call(args=args, env=env))
