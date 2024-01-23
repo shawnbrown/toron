ea0d71360eb73a3a8c5698015b7c586791662c42
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-01-23 11:20:51 -0500

    Clean-up run-tests.py and include `cwd` when calling subprocess.

--- run-tests.py
+++ run-tests.py
@@ -5,18 +5,20 @@ import sys
 
 
 args = [
-    sys.executable or 'python',       # Python interpreter to call for testing.
-    '-B',                             # Don't write .pyc files on import.
-    '-O',                             # Remove assert and __debug__-dependent statements.
-    '-W', 'default',                  # Enable default handling for all warnings.
-    '-m', 'unittest',                 # Run the unittest module as a script.
-    'discover',                       # Use test discovery.
-    '-s', 'tests',                    # Start discovery in 'tests' directory.
-    '-t', os.path.dirname(__file__),  # Set top-level of project.
+    sys.executable or 'python',  # Get Python interpreter to call for testing.
+    '-B',                        # Don't write .pyc files on import.
+    '-O',                        # Remove assert and __debug__-dependent statements.
+    '-W', 'default',             # Enable default handling for all warnings.
+    '-m', 'unittest',            # Run the unittest module as a script.
+    'discover',                  # Use test discovery.
+    '-s', 'tests',               # Start discovery in 'tests' directory.
+    '-t', '.',                   # Set top level of project to working directory.
 ]
 args.extend(sys.argv[1:])  # Append any arguments passed to script (-v, etc.).
 
-env = os.environ.copy()
-env['PYTHONPATH'] = 'src'  # Make package in 'src' importable.
+cwd = os.path.dirname(__file__) or '.'  # Get working directory for subprocess.
 
-sys.exit(subprocess.call(args=args, env=env))
+env = os.environ.copy()
+env['PYTHONPATH'] = 'src'  # Make package importable from 'src' directory.
+
+sys.exit(subprocess.call(args=args, cwd=cwd, env=env))
