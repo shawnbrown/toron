7330c9c8a076e4a86f4ea8a894edf346b380e2d4
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-02-07 03:03:43 -0500

    Update run-tests.py to accept optional command line options and tests.

--- run-tests.py
+++ run-tests.py
@@ -10,11 +10,10 @@ args = [
     '-O',                        # Remove assert and __debug__-dependent statements.
     '-W', 'default',             # Enable default handling for all warnings.
     '-m', 'unittest',            # Run the unittest module as a script.
-    'discover',                  # Use test discovery.
-    '-s', 'tests',               # Start discovery in 'tests' directory.
-    '-t', '.',                   # Set top level of project to working directory.
 ]
-args.extend(sys.argv[1:])  # Append any arguments passed to script (-v, etc.).
+
+# Append arguments passed to script or configure test discovery.
+args.extend(sys.argv[1:] or ['discover', '-s', 'tests', '-t', '.'])
 
 cwd = os.path.dirname(__file__) or '.'  # Get working directory for subprocess.
 
