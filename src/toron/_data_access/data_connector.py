b8bb467941679243d94298959d56733a695ce61c
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-02-26 02:59:18 -0500

    Add temp-file creation and `_current_working_path` handling.

--- src/toron/_data_access/data_connector.py
+++ src/toron/_data_access/data_connector.py
@@ -1,8 +1,26 @@
 """DataConnector and related objects using SQLite."""
 
+import os
+import tempfile
+
+from toron._typing import (
+    Optional,
+)
+
 from .base_classes import BaseDataConnector
 
 
 class DataConnector(BaseDataConnector):
+    # Absolute path of class instance's database (None if file in memory).
+    _current_working_path: Optional[str] = None
+
     def __init__(self, cache_to_drive: bool = False) -> None:
         """Initialize a new node instance."""
+        if cache_to_drive:
+            temp_f = tempfile.NamedTemporaryFile(suffix='.toron', delete=False)
+            temp_f.close()
+            database_path = os.path.abspath(temp_f.name)
+            self._current_working_path = database_path
+        else:
+            database_path = ':memory:'
+            self._current_working_path = None
--- tests/data_access/test_data_connector.py
+++ tests/data_access/test_data_connector.py
@@ -1,5 +1,6 @@
 """Tests for toron/_data_access/data_connector.py module."""
 
+import tempfile
 import unittest
 
 from toron._data_access.base_classes import BaseDataConnector
@@ -9,3 +10,12 @@ from toron._data_access.data_connector import DataConnector
 class TestDataConnector(unittest.TestCase):
     def test_inheritance(self):
         self.assertTrue(issubclass(DataConnector, BaseDataConnector))
+
+    def test_current_working_path(self):
+        connector = DataConnector()  # <- Creates in-memory database.
+        self.assertIsNone(connector._current_working_path)
+
+        connector = DataConnector(cache_to_drive=True)  # <- Creates on-drive database.
+        tempdir = tempfile.gettempdir()
+        self.assertTrue(connector._current_working_path.startswith(tempdir))
+        self.assertTrue(connector._current_working_path.endswith('.toron'))
