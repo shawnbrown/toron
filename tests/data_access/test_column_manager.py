dcd41d1c53b55e2b4fcd78d8921c2fd503e212e3
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-03-26 03:45:00 -0400

    Update TestColumnManager abstract base class.

--- tests/data_access/test_column_manager.py
+++ tests/data_access/test_column_manager.py
@@ -5,6 +5,7 @@ import unittest
 from abc import ABC, abstractmethod
 from types import SimpleNamespace
 
+from toron._data_access.data_connector import DataConnector
 from toron._data_access.base_classes import BaseColumnManager
 from toron._data_access.column_manager import ColumnManager
 
@@ -19,9 +20,32 @@ class Bases(SimpleNamespace):
             """The concrete class to be tested."""
             return NotImplemented
 
+        def setUp(self):
+            connector = DataConnector()
+            resource = connector.acquire_resource()
+            self.addCleanup(lambda: connector.release_resource(resource))
+
+            self.cursor = resource.cursor()
+
         def test_inheritance(self):
             """Should subclass from appropriate abstract base class."""
             self.assertTrue(issubclass(self.concrete_class, BaseColumnManager))
+
+        @abstractmethod
+        def test_add_columns(self):
+            ...
+
+        @abstractmethod
+        def test_get_columns(self):
+            ...
+
+        @abstractmethod
+        def test_update_columns(self):
+            ...
+
+        @abstractmethod
+        def test_delete_columns(self):
+            ...
 
 
 class TestColumnManager(Bases.TestColumnManager):
