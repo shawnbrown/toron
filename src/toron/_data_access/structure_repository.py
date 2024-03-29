97bae8a21f2351c6324ba3b1b5946c04502d471e
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-03-29 14:29:36 -0400

    Add concrete StructureRepository class and related tests.

---
+++ src/toron/_data_access/structure_repository.py
@@ -0,0 +1,54 @@
+"""StructureRepository and related objects using SQLite."""
+
+import sqlite3
+
+from toron._typing import Optional
+
+from .base_classes import Structure, BaseStructureRepository
+
+
+class StructureRepository(BaseStructureRepository):
+    def __init__(self, data_reader: sqlite3.Cursor) -> None:
+        """Initialize a new StructureRepository instance."""
+        self._cursor = data_reader
+
+    def add(self, value: str, *values: str) -> None:
+        """Add a record to the repository."""
+        values = (value,) + values
+        qmarks = ', '.join('?' * len(values))
+        sql = f'INSERT INTO main.structure VALUES (NULL, NULL, {qmarks})'
+        self._cursor.execute(sql, values)
+
+    def get(self, id: int) -> Optional[Structure]:
+        """Get a record from the repository."""
+        self._cursor.execute(
+            'SELECT * FROM main.structure WHERE _structure_id=?', (id,)
+        )
+        record = self._cursor.fetchone()
+        if record:
+            return Structure(*record)
+        return None
+
+    #@abstractmethod
+    #def get_all(self) -> Iterable[Structure]:
+    #    """Get all records sorted from most to least granular."""
+    #    raise NotImplementedError
+
+    def update(self, record: Structure) -> None:
+        """Update a record in the repository."""
+        self._cursor.execute(f"PRAGMA main.table_info('structure')")
+        columns = ', '.join(row[1] for row in self._cursor.fetchall()[2:])
+        qmarks = ', '.join('?' * len(record.values))
+        sql = f"""
+            UPDATE main.structure
+            SET (_granularity, {columns}) = (?, {qmarks})
+            WHERE _structure_id=?
+        """
+        parameters = (record.granularity,) + record.values + (record.id,)
+        self._cursor.execute(sql, parameters)
+
+    def delete(self, id: int):
+        """Delete a record from the repository."""
+        self._cursor.execute(
+            'DELETE FROM main.structure WHERE _structure_id=?', (id,)
+        )
--- tests/data_access/test_structure_repository.py
+++ tests/data_access/test_structure_repository.py
@@ -7,7 +7,7 @@ from types import SimpleNamespace
 
 from toron._data_access.data_connector import DataConnector
 from toron._data_access.base_classes import Structure, BaseStructureRepository
-#from toron._data_access.structure_repository import StructureRepository
+from toron._data_access.structure_repository import StructureRepository
 
 
 class Bases(SimpleNamespace):
@@ -51,3 +51,99 @@ class Bases(SimpleNamespace):
         @abstractmethod
         def test_delete(self):
             ...
+
+
+class TestStructureRepository(Bases.TestStructureRepository):
+    @property
+    def repository_class(self):
+        return StructureRepository
+
+    def setUp(self):
+        super().setUp()
+        self.cursor.executescript("""
+            DROP INDEX IF EXISTS unique_structure_label_columns;
+            ALTER TABLE structure ADD COLUMN "A" INTEGER NOT NULL CHECK ("A" IN (0, 1)) DEFAULT 0;
+            ALTER TABLE structure ADD COLUMN "B" INTEGER NOT NULL CHECK ("B" IN (0, 1)) DEFAULT 0;
+            ALTER TABLE structure ADD COLUMN "C" INTEGER NOT NULL CHECK ("C" IN (0, 1)) DEFAULT 0;
+            CREATE UNIQUE INDEX unique_structure_label_columns ON structure("A", "B", "C");
+        """)
+
+    def assertRecords(self, expected_records, msg=None):
+        self.cursor.execute(f'SELECT * FROM structure')
+        actual_records = self.cursor.fetchall()
+        self.assertEqual(actual_records, expected_records, msg=msg)
+
+    def test_add(self):
+        repository = StructureRepository(self.cursor)
+
+        repository.add(0, 0, 0)
+        repository.add(1, 1, 0)
+        repository.add(1, 1, 1)
+
+        self.assertRecords([(1, None, 0, 0, 0), (2, None, 1, 1, 0), (3, None, 1, 1, 1)])
+
+        msg = "should not add (1, 1, 1) again, duplicates not allowed"
+        with self.assertRaises(sqlite3.IntegrityError, msg=msg):
+            repository.add(1, 1, 1)
+
+        msg = "NULL values not allowed in label columns"
+        with self.assertRaises(sqlite3.IntegrityError, msg=msg):
+            repository.add(1, None, 1)
+
+    def test_get(self):
+        repository = StructureRepository(self.cursor)
+        self.cursor.executescript("""
+            INSERT INTO structure VALUES (1, NULL, 0, 0, 0);
+            INSERT INTO structure VALUES (2, NULL, 1, 1, 0);
+            INSERT INTO structure VALUES (3, NULL, 1, 1, 1);
+        """)
+
+        self.assertEqual(repository.get(1), Structure(1, None, 0, 0, 0))
+        self.assertEqual(repository.get(2), Structure(2, None, 1, 1, 0))
+        self.assertEqual(repository.get(3), Structure(3, None, 1, 1, 1))
+        self.assertIsNone(repository.get(37), msg='should be None if no matching id')
+        self.assertIsNone(repository.get(0), msg='should be None if no matching id')
+
+    @unittest.skip('not implemented')
+    def test_get_all(self):
+        raise NotImplementedError
+
+    def test_update(self):
+        repository = StructureRepository(self.cursor)
+        self.cursor.executescript("""
+            INSERT INTO structure VALUES (1, NULL, 0, 0, 0);
+            INSERT INTO structure VALUES (2, NULL, 1, 1, 0);
+            INSERT INTO structure VALUES (3, NULL, 1, 1, 1);
+        """)
+
+        repository.update(Structure(3, 9.25, 1, 1, 1))
+        self.cursor.execute('SELECT * FROM structure')
+        records = self.cursor.fetchall()
+        self.assertEqual(records, [(1, None, 0, 0, 0), (2, None, 1, 1, 0), (3, 9.25, 1, 1, 1)])
+
+        with self.assertRaises(sqlite3.OperationalError, msg='4 columns but only 2 values'):
+            repository.update(Structure(3, 9.25, 1))
+
+        repository.update(Structure(7, 3.5, 0, 0, 1))  # <- No _structure_id 7 exists.
+        self.cursor.execute('SELECT * FROM structure')
+        records = self.cursor.fetchall()
+        msg = 'there is no _structure_id 7, records should be unchanged'
+        self.assertEqual(records, [(1, None, 0, 0, 0), (2, None, 1, 1, 0), (3, 9.25, 1, 1, 1)], msg=msg)
+
+    def test_delete(self):
+        repository = StructureRepository(self.cursor)
+        self.cursor.executescript("""
+            INSERT INTO structure VALUES (1, 0.0, 0, 0, 0);
+            INSERT INTO structure VALUES (2, 9.25, 1, 1, 1);
+        """)
+
+        repository.delete(1)
+        self.assertRecords([(2, 9.25, 1, 1, 1)])
+
+        repository.delete(2)
+        self.assertRecords([])
+
+        try:
+            repository.delete(42)
+        except Exception as err:
+            self.fail(f'deleting non-existant ids should not raise errors, got {err!r}')
