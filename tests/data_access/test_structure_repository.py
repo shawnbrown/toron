139444f968dd9d2abc4aaa1c03216f3d1dc6f3ba
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-03-30 00:58:26 -0400

    Consolidate StructureRepository base test into concrete test.
    The abstract base test wasn't doing enough on its own to warrant
    the added complexity.

--- tests/data_access/test_structure_repository.py
+++ tests/data_access/test_structure_repository.py
@@ -2,64 +2,21 @@
 
 import sqlite3
 import unittest
-from abc import ABC, abstractmethod
-from types import SimpleNamespace
 
 from toron._data_access.data_connector import DataConnector
 from toron._data_access.base_classes import Structure, BaseStructureRepository
 from toron._data_access.structure_repository import StructureRepository
 
 
-class Bases(SimpleNamespace):
-    """Wrapping TestCase base classes to prevent test discovery."""
-
-    class TestStructureRepository(ABC, unittest.TestCase):
-        @property
-        @abstractmethod
-        def repository_class(self):
-            """The concrete class to be tested."""
-            return NotImplemented
-
-        def setUp(self):
-            connector = DataConnector()
-            resource = connector.acquire_resource()
-            self.addCleanup(lambda: connector.release_resource(resource))
-
-            self.cursor = resource.cursor()
-            self.addCleanup(self.cursor.close)
-
-        def test_inheritance(self):
-            """Should subclass from appropriate abstract base class."""
-            self.assertTrue(issubclass(self.repository_class, BaseStructureRepository))
-
-        @abstractmethod
-        def test_add(self):
-            ...
-
-        @abstractmethod
-        def test_get(self):
-            ...
-
-        @abstractmethod
-        def test_get_all(self):
-            ...
-
-        @abstractmethod
-        def test_update(self):
-            ...
-
-        @abstractmethod
-        def test_delete(self):
-            ...
-
-
-class TestStructureRepository(Bases.TestStructureRepository):
-    @property
-    def repository_class(self):
-        return StructureRepository
-
+class TestStructureRepository(unittest.TestCase):
     def setUp(self):
-        super().setUp()
+        connector = DataConnector()
+        resource = connector.acquire_resource()
+        self.addCleanup(lambda: connector.release_resource(resource))
+
+        self.cursor = resource.cursor()
+        self.addCleanup(self.cursor.close)
+
         self.cursor.executescript("""
             DROP INDEX IF EXISTS unique_structure_label_columns;
             ALTER TABLE structure ADD COLUMN "A" INTEGER NOT NULL CHECK ("A" IN (0, 1)) DEFAULT 0;
@@ -67,6 +24,10 @@ class TestStructureRepository(Bases.TestStructureRepository):
             ALTER TABLE structure ADD COLUMN "C" INTEGER NOT NULL CHECK ("C" IN (0, 1)) DEFAULT 0;
             CREATE UNIQUE INDEX unique_structure_label_columns ON structure("A", "B", "C");
         """)
+
+    def test_inheritance(self):
+        """Should subclass from appropriate abstract base class."""
+        self.assertTrue(issubclass(StructureRepository, BaseStructureRepository))
 
     def assertRecords(self, expected_records, msg=None):
         self.cursor.execute(f'SELECT * FROM structure')
