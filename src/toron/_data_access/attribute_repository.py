d2145ec64abac88a308ab250b524a43e50cac5e3
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-03-31 14:20:16 -0400

    Add AttributeRepository concrete class and tests.

---
+++ src/toron/_data_access/attribute_repository.py
@@ -0,0 +1,45 @@
+"""AttributeRepository and related objects using SQLite."""
+
+import sqlite3
+from json import dumps as json_dumps
+
+from toron._typing import (
+    Dict,
+    Optional,
+)
+
+from .base_classes import Attribute, BaseAttributeRepository
+
+
+class AttributeRepository(BaseAttributeRepository):
+    def __init__(self, data_reader: sqlite3.Cursor) -> None:
+        """Initialize a new repository instance."""
+        self._cursor = data_reader
+
+    def add(self, value: Dict[str, str]) -> None:
+        """Add a record to the repository."""
+        sql = 'INSERT INTO main.attribute (attribute_value) VALUES (?)'
+        self._cursor.execute(sql, (json_dumps(value, sort_keys=True),))
+
+    def get(self, id: int) -> Optional[Attribute]:
+        """Get a record from the repository."""
+        self._cursor.execute(
+            'SELECT * FROM main.attribute WHERE attribute_id=?', (id,)
+        )
+        record = self._cursor.fetchone()
+        if record:
+            return Attribute(*record)
+        return None
+
+    def update(self, record: Attribute) -> None:
+        """Update a record in the repository."""
+        self._cursor.execute(
+            'UPDATE main.attribute SET attribute_value=? WHERE attribute_id=?',
+            (json_dumps(record.value, sort_keys=True), record.id),
+        )
+
+    def delete(self, id: int):
+        """Delete a record from the repository."""
+        self._cursor.execute(
+            'DELETE FROM main.attribute WHERE attribute_id=?', (id,)
+        )
---
+++ tests/data_access/test_attribute_repository.py
@@ -0,0 +1,105 @@
+"""Tests for toron/_data_access/attribute_repository.py module."""
+
+import sqlite3
+import unittest
+from collections import OrderedDict
+
+from toron._data_access.data_connector import DataConnector
+from toron._data_access.base_classes import Attribute
+from toron._data_access.attribute_repository import AttributeRepository
+
+
+class TestAttributeRepository(unittest.TestCase):
+    def setUp(self):
+        connector = DataConnector()
+        resource = connector.acquire_resource()
+        self.addCleanup(lambda: connector.release_resource(resource))
+
+        self.cursor = resource.cursor()
+        self.addCleanup(self.cursor.close)
+
+    def assertRecords(self, expected_records, msg=None):
+        self.cursor.execute(f'SELECT * FROM attribute')
+        actual_records = self.cursor.fetchall()
+        self.assertEqual(actual_records, expected_records, msg=msg)
+
+    def test_add(self):
+        repository = AttributeRepository(self.cursor)
+
+        repository.add({'aaa': 'A', 'bbb': 'B'})
+        repository.add({'aaa': 'A', 'ccc': 'C'})
+
+        self.assertRecords([
+            (1, {'aaa': 'A', 'bbb': 'B'}),
+            (2, {'aaa': 'A', 'ccc': 'C'}),
+        ])
+
+        msg = "should fail, {'aaa': 'A', 'bbb': 'B'} already exists"
+        with self.assertRaises(sqlite3.IntegrityError, msg=msg):
+            repository.add(OrderedDict([('aaa', 'A'), ('bbb', 'B')]))
+
+        msg = "should fail, attr already exists, key order should be normalized"
+        with self.assertRaises(sqlite3.IntegrityError, msg=msg):
+            repository.add(OrderedDict([('bbb', 'B'), ('aaa', 'A')]))
+
+        msg = "should be dict with str keys and str values"
+        with self.assertRaises(sqlite3.IntegrityError, msg=msg):
+            repository.add({'aaa': 1})
+
+        # NOTE!: When a Python dict is converted to a JSON object,
+        # all of its keys are coerced to strings.
+        repository.add({444: 'D', 555: 'E'})  # <- Keys are integers.
+        self.assertRecords([
+            (1, {'aaa': 'A', 'bbb': 'B'}),
+            (2, {'aaa': 'A', 'ccc': 'C'}),
+            (3, {'444': 'D', '555': 'E'}),  # <- Keys are now strings!
+        ])
+
+    def test_get(self):
+        self.cursor.executescript("""
+            INSERT INTO attribute VALUES (1, '{"aaa": "A", "bbb": "B"}');
+            INSERT INTO attribute VALUES (2, '{"aaa": "A", "ccc": "C"}');
+        """)
+        repository = AttributeRepository(self.cursor)
+
+        self.assertEqual(repository.get(1), Attribute(1, {'aaa': 'A', 'bbb': 'B'}))
+        self.assertEqual(repository.get(2), Attribute(2, {'aaa': 'A', 'ccc': 'C'}))
+        self.assertIsNone(repository.get(3))
+
+    def test_update(self):
+        self.cursor.executescript("""
+            INSERT INTO attribute VALUES (1, '{"aaa": "A", "bbb": "B"}');
+            INSERT INTO attribute VALUES (2, '{"aaa": "A", "ccc": "C"}');
+        """)
+        repository = AttributeRepository(self.cursor)
+
+        repository.update(Attribute(1, {'xxx': 'X', 'zzz': 'Z'}))
+
+        expected = [
+            (1, {'xxx': 'X', 'zzz': 'Z'}),  # <- Value modified.
+            (2, {'aaa': 'A', 'ccc': 'C'}),
+        ]
+        self.assertRecords(expected)
+
+        repository.update(Attribute(3, {'yyy': 'Y'}))  # No attribute_id=3.
+
+        self.assertRecords(
+            expected,
+            msg='should be unchanged, there is no attribute_id=3',
+        )
+
+    def test_delete(self):
+        self.cursor.executescript("""
+            INSERT INTO attribute VALUES (1, '{"aaa": "A", "bbb": "B"}');
+            INSERT INTO attribute VALUES (2, '{"aaa": "A", "ccc": "C"}');
+        """)
+        repository = AttributeRepository(self.cursor)
+
+        repository.delete(1)
+        self.assertRecords([(2, {'aaa': 'A', 'ccc': 'C'})])
+
+        repository.delete(2)
+        self.assertRecords([])
+
+        repository.delete(3)  # No attribute_id=3, should pass without error.
+        self.assertRecords([])
