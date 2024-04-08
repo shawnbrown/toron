3019da703c88ee9b6526b7319ffaef7db3f01cd5
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-04-07 22:41:27 -0400

    Add tests for instantiating Node class.

--- tests/test_node.py
+++ tests/test_node.py
@@ -5,6 +5,30 @@ import unittest
 from toron._node import Node
 
 
-class TestNode(unittest.TestCase):
-    def test_stub(self):
-        self.node = Node()
+class TestInstantiation(unittest.TestCase):
+    def test_backend_implicit(self):
+        """When no arguments are given, should create empty node."""
+        node = Node()
+        self.assertEqual(node._dal.backend, 'DAL1')
+
+    def test_backend_explicit(self):
+        """The ``backend`` can be given explicitly."""
+        node = Node(backend='DAL1')
+        self.assertEqual(node._dal.backend, 'DAL1')
+
+    def test_backend_keyword_only(self):
+        """The ``backend`` argument is keyword-only (not positional)."""
+        with self.assertRaises(TypeError):
+            node = Node('DAL1')  # Using positional argument.
+
+    def test_backend_unknown(self):
+        """Invalid ``backend`` values should raise an error."""
+        with self.assertRaises(RuntimeError):
+            node = Node(backend=None)
+
+        with self.assertRaises(RuntimeError):
+            node = Node(backend='DAL#')
+
+    def test_kwds(self):
+        """The ``**kwds`` are used to create a DataConnector."""
+        node = Node(cache_to_drive=True)
