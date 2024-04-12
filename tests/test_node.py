41a032f176ad7df8cc8fde39d25b9f701bda6d1e
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-04-11 21:59:11 -0400

    Tidy-up managed cursor tests using sentinel objects.

--- tests/test_node.py
+++ tests/test_node.py
@@ -107,20 +107,18 @@ class TestManagedConnectionCursorAndTransaction(unittest.TestCase):
         """
         node = Node()
         node._connector = Mock()
-        dummy_connections = [object(), object()]
+        dummy_connections = [sentinel.con1, sentinel.con2]
         node._connector.acquire_connection.side_effect = dummy_connections
 
         with node._managed_cursor() as cursor:  # <- No `connection` passed.
             node._connector.assert_has_calls([
                 call.acquire_connection(),  # <- Connection acquired automatically.
-                call.acquire_cursor(dummy_connections[0]),  # <- Cursor acquired.
+                call.acquire_cursor(sentinel.con1),  # <- Cursor acquired.
             ])
 
         node._connector.assert_has_calls([
-            call.acquire_connection(),
-            call.acquire_cursor(dummy_connections[0]),
             call.release_cursor(cursor),  # <- Cursor released.
-            call.release_connection(dummy_connections[0]),  # <- Connection released.
+            call.release_connection(sentinel.con1),  # <- Connection released.
         ])
 
     def test_managed_transaction(self):
