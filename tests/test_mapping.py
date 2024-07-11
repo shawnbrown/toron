84e56de5a793c691111b1b432eda9ed61aba0228
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-07-11 16:05:13 -0400

    Update tests to move SQL queries into a common helper method.

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -7,6 +7,12 @@ from toron.mapper import Mapper
 
 
 class TestMapperInit(unittest.TestCase):
+    @staticmethod
+    def get_mapping_data(mapper):
+        """Helper method to get contents of 'mapping_data' table."""
+        mapper.cur.execute('SELECT * FROM mapping_data')
+        return set(mapper.cur.fetchall())
+
     def test_exact_crosswalk_name(self):
         """Test crosswalk name matches value column exactly."""
         data = [
@@ -21,13 +27,11 @@ class TestMapperInit(unittest.TestCase):
 
         self.assertEqual(mapper.left_keys, ['idx1'])
         self.assertEqual(mapper.right_keys, ['idx1', 'idx2'])
-
-        mapper.cur.execute('SELECT * FROM mapping_data')
-        expected = {
-            (1, '["A"]', '["A", "x"]', 70.0),
-            (2, '["B"]', '["B", "y"]', 80.0),
-        }
-        self.assertEqual(set(mapper.cur.fetchall()), expected)
+        self.assertEqual(
+            self.get_mapping_data(mapper),
+            {(1, '["A"]', '["A", "x"]', 70.0),
+             (2, '["B"]', '["B", "y"]', 80.0)},
+        )
 
     def test_parsed_crosswalk_name(self):
         """Test crosswalk name parsed from shorthand-syntax."""
@@ -43,13 +47,11 @@ class TestMapperInit(unittest.TestCase):
 
         self.assertEqual(mapper.left_keys, ['idx1'])
         self.assertEqual(mapper.right_keys, ['idx1', 'idx2'])
-
-        mapper.cur.execute('SELECT * FROM mapping_data')
-        expected = {
-            (1, '["A"]', '["A", "x"]', 70.0),
-            (2, '["B"]', '["B", "y"]', 80.0),
-        }
-        self.assertEqual(set(mapper.cur.fetchall()), expected)
+        self.assertEqual(
+            self.get_mapping_data(mapper),
+            {(1, '["A"]', '["A", "x"]', 70.0),
+             (2, '["B"]', '["B", "y"]', 80.0)},
+        )
 
     def test_empty_rows_in_data(self):
         """Empty rows should be skipped."""
@@ -61,9 +63,8 @@ class TestMapperInit(unittest.TestCase):
         ]
         mapper = Mapper('population', data)
 
-        mapper.cur.execute('SELECT * FROM mapping_data')
-        expected = {
-            (1, '["A"]', '["A", "x"]', 70.0),
-            (2, '["B"]', '["B", "y"]', 80.0),
-        }
-        self.assertEqual(set(mapper.cur.fetchall()), expected)
+        self.assertEqual(
+            self.get_mapping_data(mapper),
+            {(1, '["A"]', '["A", "x"]', 70.0),
+             (2, '["B"]', '["B", "y"]', 80.0)},
+        )
