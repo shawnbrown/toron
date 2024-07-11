2e7b9626e711112ceec434d32cd65e8de3022d9d
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-07-11 04:20:21 -0400

    Add `__init__()`, `close()`, and `__del__()` methods to Mapper class.

--- src/toron/mapper.py
+++ src/toron/mapper.py
@@ -1,5 +1,10 @@
 """Tools for building weighted crosswalks between sets of labels."""
 
+import sqlite3
+from json import (
+    dumps,
+)
+
 from ._typing import (
     Dict,
     Iterable,
@@ -8,13 +13,100 @@ from ._typing import (
     Union,
 )
 
+from ._utils import (
+    normalize_tabular,
+)
+
 
 class Mapper(object):
-    """Class to build a weighted crosswalk between sets of labels."""
+    """Class to build a weighted crosswalk between sets of labels.
+
+    This class create a temporary database--when an instance is garbage
+    collected, its database is deleted. It uses the following schema:
+
+    .. code-block:: text
+
+        +---------------+    +---------------+    +---------------+
+        | left_matches  |    | mapping_data  |    | right_matches |
+        +---------------+    +---------------+    +---------------+
+        | run_id        |<---| run_id        |--->| run_id        |
+        | index_id      |    | left_labels   |    | index_id      |
+        | weight_value  |    | right_labels  |    | weight_value  |
+        | mapping_level |    | mapping_value |    | mapping_level |
+        | proportion    |    +---------------+    | proportion    |
+        +---------------+                         +---------------+
+    """
     def __init__(
         self,
         crosswalk_name: str,
         data: Union[Iterable[Sequence], Iterable[Dict]],
         columns: Optional[Sequence[str]] = None,
     ) -> None:
-        pass
+        self.con = sqlite3.connect('')  # Empty string creates temp file.
+        self.cur = self.con.executescript("""
+            CREATE TABLE mapping_data(
+                run_id INTEGER PRIMARY KEY,
+                left_labels TEXT NOT NULL,
+                right_labels TEXT NOT NULL,
+                mapping_value REAL NOT NULL
+            );
+            CREATE TABLE left_matches(
+                run_id INTEGER NOT NULL REFERENCES mapping_data(run_id),
+                index_id INTEGER,
+                weight_value REAL CHECK (0.0 <= weight_value),
+                mapping_level BLOB_BITFLAGS,
+                proportion REAL CHECK (0.0 <= proportion AND proportion <= 1.0)
+            );
+            CREATE TABLE right_matches(
+                run_id INTEGER NOT NULL REFERENCES mapping_data(run_id),
+                index_id INTEGER,
+                weight_value REAL CHECK (0.0 <= weight_value),
+                mapping_level BLOB_BITFLAGS,
+                proportion REAL CHECK (0.0 <= proportion AND proportion <= 1.0)
+            );
+        """)
+
+        data, columns = normalize_tabular(data, columns)
+
+        for i, col in enumerate(columns):
+            if crosswalk_name == col:
+                value_pos = i  # Get index position of value column
+                break
+        else:  # no break
+            msg = f'{crosswalk_name!r} is not in data, got header: {columns!r}'
+            raise ValueError(msg)
+
+        self.left_keys = columns[:value_pos]
+        self.right_keys = columns[value_pos+1:]
+
+        for row in data:
+            if not row:
+                continue  # If row is empty, skip to next.
+
+            sql = """
+                INSERT INTO mapping_data
+                  (left_labels, right_labels, mapping_value)
+                  VALUES (:left_labels, :right_labels, :mapping_value)
+            """
+            parameters = {
+                'left_labels': dumps(row[:value_pos]),
+                'right_labels': dumps(row[value_pos+1:]),
+                'mapping_value': row[value_pos],
+            }
+            try:
+                self.cur.execute(sql, parameters)
+            except sqlite3.IntegrityError as err:
+                msg = f'{err}\nfailed to insert:\n  {tuple(parameters.values())}'
+                raise sqlite3.IntegrityError(msg) from err
+
+    def close(self) -> None:
+        """Close internal connection to temporary database."""
+        try:
+            self.cur.close()  # Fails if Connection is not open.
+        except sqlite3.ProgrammingError:
+            pass
+
+        self.con.close()
+
+    def __del__(self) -> None:
+        self.close()
--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -7,4 +7,25 @@ from toron.mapper import Mapper
 
 
 class TestMapperInit(unittest.TestCase):
-    pass
+    def test_exact_crosswalk_name(self):
+        """Test crosswalk name matches value column exactly."""
+        data = [
+            ['idx1', 'population', 'idx1', 'idx2'],
+            ['A', 70, 'A', 'x'],
+            ['B', 80, 'B', 'y'],
+            [],  # <- Empty row to simulate trailing newline from text file input.
+        ]
+        mapper = Mapper(
+            crosswalk_name='population',  # <- Matches name of column exactly.
+            data=data,
+        )
+
+        self.assertEqual(mapper.left_keys, ['idx1'])
+        self.assertEqual(mapper.right_keys, ['idx1', 'idx2'])
+
+        mapper.cur.execute('SELECT * FROM mapping_data')
+        expected = {
+            (1, '["A"]', '["A", "x"]', 70.0),
+            (2, '["B"]', '["B", "y"]', 80.0),
+        }
+        self.assertEqual(set(mapper.cur.fetchall()), expected)
