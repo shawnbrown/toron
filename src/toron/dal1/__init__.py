45bc333d899307630582e9aa056b3baf921cef96
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-10-06 01:58:20 -0400

    Add a check for the SQLite version to 'dal1.__init__.py'.

--- src/toron/dal1/__init__.py
+++ src/toron/dal1/__init__.py
@@ -1,5 +1,15 @@
 """Back-end implementation for data access API using SQLite."""
 
+if __import__('sqlite3').sqlite_version_info < (3, 21, 0):
+    raise RuntimeError(
+        f"This build of Python {__import__('platform').python_version()} "
+        f"is bundled with SQLite {__import__('sqlite3').sqlite_version}. "
+        f"But Toron's \"DAL1\" backend requires SQLite 3.21.0 or newer.\n"
+        f"\n"
+        f"Please use an updated Python build with a newer version of SQLite."
+    )
+
+
 from .data_connector import DataConnector
 from .column_manager import (
     ColumnManager,
