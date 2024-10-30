aa7d40325310b192a22d8e9507292637b4d280a3
Shawn Brown <shawnbrown@users.noreply.github.com>
2024-10-30 13:54:56 -0400

    Update CLI to have more direct control of output.

--- src/toron/cli/main.py
+++ src/toron/cli/main.py
@@ -8,6 +8,7 @@ from shutil import get_terminal_size
 from .._typing import (
     Final,
 )
+from ..data_service import get_node_info_text
 from .. import (
     __version__,
     Node,
@@ -52,8 +53,33 @@ def main() -> int:
     # Define horizontal rule `hr` made from "Box Drawings" character.
     hr = '─' * min(len(filename), (get_terminal_size()[0] - 1))
 
+    # Get dictionary of node info values.
+    with node._managed_cursor() as cursor:
+        info_dict = get_node_info_text(
+            property_repo=node._dal.PropertyRepository(cursor),
+            column_manager=node._dal.ColumnManager(cursor),
+            structure_repo=node._dal.StructureRepository(cursor),
+            weight_group_repo=node._dal.WeightGroupRepository(cursor),
+            attribute_repo=node._dal.AttributeGroupRepository(cursor),
+            crosswalk_repo=node._dal.CrosswalkRepository(cursor),
+        )
+
+    # Prepare and write output.
+    domain_str = '\n  '.join(info_dict['domain_list'])
+    crosswalks_str = '\n  '.join(info_dict['crosswalks_list'])
     sys.stdout.write(
-        f'{hr}\n{filename}\n{hr}\n'
-        f'{repr(node)}\n'
+        f"{hr}\n{filename}\n{hr}\n"
+        f"domain:\n"
+        f"  {domain_str}\n"
+        f"index:\n"
+        f"  {', '.join(info_dict['index_list'])}\n"
+        f"granularity:\n"
+        f"  {info_dict['granularity_str']}\n"
+        f"weights:\n"
+        f"  {', '.join(info_dict['weights_list'])}\n"
+        f"attributes:\n"
+        f"  {', '.join(info_dict['attribute_list'])}\n"
+        f"incoming crosswalks:\n"
+        f"  {crosswalks_str}\n"
     )
     return EXITCODE_OK
