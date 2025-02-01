e6abed7dfb22f55f49a1a7790e9b3a4babb046d5
Shawn Brown <shawnbrown@users.noreply.github.com>
2025-02-01 02:45:09 -0500

    Change check_dependencies.py to remove `unittest` code.
    Since there was only one test case, it didn't make sense
    to run this check as a test suite.

--- tests/check_dependencies.py
+++ tests/check_dependencies.py
@@ -1,4 +1,4 @@
-"""Tests for installed package dependencies.
+"""Verify package dependencies.
 
 To prevent the addition of transitive dependencies from going unnoticed,
 we check the packages that get installed when testing via `tox`.
@@ -7,44 +7,77 @@ we check the packages that get installed when testing via `tox`.
 import os
 import subprocess
 import sys
-import unittest
 
 
-@unittest.skipUnless(os.getenv('TOX_ENV_NAME'), 'requires tox environment')
-class TestPackageDependencies(unittest.TestCase):
-    def get_installed_packages(self):
-        """Return installed package names as a set of strings."""
-        if not sys.executable:
-            self.fail(
-                'cannot determine executable binary for current '
-                'Python interpreter, `sys.executable` is empty'
-            )
+EXITCODE_OK = 0
+EXITCODE_ERR = 1
 
-        result = subprocess.run(
-            args=[
-                sys.executable,        # Execute current Python interpreter.
-                '-m', 'pip',           # Run pip using module-as-script interface.
-                'freeze',              # List installed packages.
-                '--local',             # Omit globally-installed packages.
-                '--exclude', 'toron',  # Exclude Toron itself.
-            ],
-            stdout=subprocess.PIPE,
-            text=True,
+
+def main(package_dependencies):
+    if not os.getenv('TOX_ENV_NAME'):
+        sys.stderr.write(
+            'This script should be run inside a tox environment.\n'
         )
-        result_text = result.stdout.strip()
-        result_list = result_text.split('\n')
-        package_names = [x.partition('==')[0] for x in result_list]
-        return set(package_names)
+        return EXITCODE_ERR  # <- EXIT!
 
-    def test_package_dependencies(self):
-        """Check for expected package dependencies."""
-        package_dependencies = {
-            'lark',
-        }
-        if sys.version_info < (3, 11):
-            package_dependencies.add('typing_extensions')
+    if not sys.executable:
+        sys.stderr.write(
+            'Cannot determine executable binary for current '
+            'Python interpreter, `sys.executable` is empty.\n'
+        )
+        return EXITCODE_ERR  # <- EXIT!
 
-        if sys.platform == 'win32':
-            package_dependencies.add('colorama')
+    result = subprocess.run(
+        args=[
+            sys.executable,        # Execute current Python interpreter.
+            '-m', 'pip',           # Run pip using module-as-script interface.
+            'freeze',              # List installed packages.
+            '--local',             # Omit globally-installed packages.
+            '--exclude', 'toron',  # Exclude Toron itself.
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    result_text = result.stdout.strip()
+    result_list = result_text.split('\n')
+    installed_packages = {x.partition('==')[0] for x in result_list}
 
-        self.assertEqual(self.get_installed_packages(), package_dependencies)
+    if installed_packages == package_dependencies:
+        sys.stdout.write(
+            f"Dependency check passed!\n"
+            f"Installed dependencies include:\n"
+            f"* {'\n* '.join(sorted(installed_packages))}\n"
+        )
+        return EXITCODE_OK  # <- EXIT!
+
+    sys.stderr.write('Dependency check failed.\n')
+
+    missing = package_dependencies - installed_packages
+    extra = installed_packages - package_dependencies
+    if missing:
+        sys.stderr.write(
+            f"\nMissing expected packages:\n"
+            f"* {'\n* '.join(sorted(missing))}\n"
+        )
+    if extra:
+        sys.stderr.write(
+            f"\nFound extra unexpected packages:\n"
+            f"* {'\n* '.join(sorted(extra))}\n"
+        )
+
+    sys.stderr.write('\n')
+    return EXITCODE_ERR
+
+
+if __name__ == '__main__':
+    package_dependencies = {
+        'lark',
+    }
+    if sys.version_info < (3, 11):
+        package_dependencies.add('typing_extensions')
+
+    if sys.platform == 'win32':
+        package_dependencies.add('colorama')
+
+    exitcode = main(package_dependencies)
+    sys.exit(exitcode)
