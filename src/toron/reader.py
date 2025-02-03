6367fa6d0e14a529ad1ce20f8aa932d9ab2d48e2
Shawn Brown <shawnbrown@users.noreply.github.com>
2025-02-03 15:18:03 -0500

    Update `NodeReader` to create database and load data.

--- src/toron/reader.py
+++ src/toron/reader.py
@@ -1,11 +1,14 @@
 """NodeReader implementation for the Toron project."""
 
 import os
+import sqlite3
 import weakref
 from contextlib import closing, suppress
+from json import dumps, loads
 from tempfile import NamedTemporaryFile
 from toron._typing import (
     Dict,
+    Iterator,
     Optional,
     Self,
     Tuple,
@@ -14,7 +17,10 @@ from toron._typing import (
 
 class NodeReader(object):
     """An iterator for base level TopoNode data."""
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        data: Iterator[Tuple[int, Dict[str, str], Optional[float]]],
+    ) -> None:
         # Create temp file and get its path (resolve symlinks with realpath).
         with closing(NamedTemporaryFile(delete=False)) as f:
             self._filepath = os.path.realpath(f.name)
@@ -22,7 +28,51 @@ class NodeReader(object):
         # Assign finalizer as a `close()` method.
         self.close = weakref.finalize(self, self._cleanup)
 
+        # Create tables and insert records.
+        with closing(sqlite3.connect(self._filepath)) as con:
+            try:
+                cur = con.executescript("""
+                    CREATE TABLE attr_data (
+                        attr_data_id INTEGER PRIMARY KEY,
+                        attributes TEXT NOT NULL,
+                        matched_crosswalk_id INTEGER DEFAULT NULL,
+                        UNIQUE (attributes)
+                    );
+                    CREATE TABLE quant_data (
+                        index_id INTEGER NOT NULL,
+                        attr_data_id INTEGER NOT NULL,
+                        quant_value REAL,
+                        FOREIGN KEY(attr_data_id) REFERENCES attr_data(attr_data_id)
+                    );
+                """)
+                for index_id, attributes, quant_value in data:
+                    attr_data_id = self._add_attr_get_id(cur, attributes)
+                    sql = """
+                        INSERT INTO main.quant_data (index_id, attr_data_id, quant_value)
+                        VALUES (?, ?, ?)
+                    """
+                    cur.execute(sql, (index_id, attr_data_id, quant_value))
+
+                con.commit()
+            except Exception:
+                con.rollback()
+                raise
+
         self._data = (_ for _ in [])
+
+    @staticmethod
+    def _add_attr_get_id(cur: sqlite3.Cursor, attributes: Dict[str, str]):
+        parameters = (dumps(attributes, sort_keys=False),)
+
+        sql = 'SELECT attr_data_id FROM attr_data WHERE attributes=?'
+        cur.execute(sql, parameters)
+        result = cur.fetchone()
+        if result:
+            return result[0]
+
+        sql = 'INSERT INTO main.attr_data (attributes) VALUES (?)'
+        cur.execute(sql, parameters)
+        return cur.lastrowid  # Row id of the last inserted row.
 
     def _cleanup(self):
         with suppress(FileNotFoundError):
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,19 +1,21 @@
 """Tests for toron/reader.py module."""
 
 import os
+import sqlite3
 import weakref
 import unittest
+from contextlib import closing
 
 from toron.reader import NodeReader
 
 
 class TestInstantiation(unittest.TestCase):
     def test_simple_case(self):
-        reader = NodeReader()
+        reader = NodeReader([])
         self.assertEqual(list(reader), [])
 
     def test_close_finalizer(self):
-        reader = NodeReader()
+        reader = NodeReader([])
 
         filepath = reader._filepath  # Get database file path.
         self.assertTrue(os.path.isfile(filepath))
@@ -22,3 +24,29 @@ class TestInstantiation(unittest.TestCase):
 
         reader.close()  # Call finalizer immediately.
         self.assertFalse(os.path.isfile(filepath))
+
+    def test_loading_data(self):
+        reader = NodeReader(
+            data=[
+                (10, {'a': 'foo'}, 25.0),
+                (11, {'a': 'foo'}, 75.0),
+                (12, {'a': 'bar'}, 50.0),
+            ],
+        )
+
+        with closing(sqlite3.connect(reader._filepath)) as con:
+            with closing(con.cursor()) as cur:
+                cur.execute('SELECT * FROM attr_data')
+                attr_data = [
+                    (1, '{"a": "foo"}', None),
+                    (2, '{"a": "bar"}', None),
+                ]
+                self.assertEqual(cur.fetchall(), attr_data)
+
+                cur.execute('SELECT * FROM quant_data')
+                quant_data = [
+                    (10, 1, 25.0),
+                    (11, 1, 75.0),
+                    (12, 2, 50.0),
+                ]
+                self.assertEqual(cur.fetchall(), quant_data)
