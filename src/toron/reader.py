66a8572e5f1514191b61f6b51b85ed5d70c0fd0a
Shawn Brown <shawnbrown@users.noreply.github.com>
2025-02-03 01:44:32 -0500

    Add temp file handling to `NodeReader`.

--- src/toron/reader.py
+++ src/toron/reader.py
@@ -1,5 +1,9 @@
 """NodeReader implementation for the Toron project."""
 
+import os
+import weakref
+from contextlib import closing, suppress
+from tempfile import NamedTemporaryFile
 from toron._typing import (
     Dict,
     Optional,
@@ -11,7 +15,18 @@ from toron._typing import (
 class NodeReader(object):
     """An iterator for base level TopoNode data."""
     def __init__(self) -> None:
+        # Create temp file and get its path (resolve symlinks with realpath).
+        with closing(NamedTemporaryFile(delete=False)) as f:
+            self._filepath = os.path.realpath(f.name)
+
+        # Assign finalizer as a `close()` method.
+        self.close = weakref.finalize(self, self._cleanup)
+
         self._data = iter([])
+
+    def _cleanup(self):
+        with suppress(FileNotFoundError):
+            os.unlink(self._filepath)
 
     def __iter__(self) -> Self:
         return self
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,5 +1,7 @@
 """Tests for toron/reader.py module."""
 
+import os
+import weakref
 import unittest
 
 from toron.reader import NodeReader
@@ -9,3 +11,14 @@ class TestInstantiation(unittest.TestCase):
     def test_simple_case(self):
         reader = NodeReader()
         self.assertEqual(list(reader), [])
+
+    def test_close_finalizer(self):
+        reader = NodeReader()
+
+        filepath = reader._filepath  # Get database file path.
+        self.assertTrue(os.path.isfile(filepath))
+
+        self.assertIsInstance(reader.close, weakref.finalize)
+
+        reader.close()  # Call finalizer immediately.
+        self.assertFalse(os.path.isfile(filepath))
