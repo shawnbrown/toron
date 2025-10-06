456ad099b60064632a2eb940bf71ea292bf512a2
Shawn Brown <shawnbrown@users.noreply.github.com>
2025-10-05 22:59:10 -0400

    Add row-count message to "index" command and adjust logger setup.

--- src/toron/cli/command_index.py
+++ src/toron/cli/command_index.py
@@ -1,11 +1,15 @@
 """Implementation for "index" command."""
 import argparse
 import csv
+import logging
 import sys
 
 from .common import ExitCode, csv_stdout_writer
 from .. import bind_node
 from ..graph import get_weights
+
+
+applogger = logging.getLogger('app-toron')
 
 
 def command(args: argparse.Namespace) -> ExitCode:
@@ -14,15 +18,17 @@ def command(args: argparse.Namespace) -> ExitCode:
     try:
         node = bind_node(path, mode='ro')
     except Exception as err:
-        import logging
-        applogger = logging.getLogger('app-toron')
         applogger.error(str(err))
         return ExitCode.ERR  # <- EXIT!
 
     weights = get_weights(node=node, weights=None, header=True)
 
+    row_count = 0
     with csv_stdout_writer() as writer:
         for row in weights:
             writer.writerow(row)
+            row_count += 1
+
+    applogger.info(f"written {row_count} row{'s' if row_count != 1 else ''}")
 
     return ExitCode.OK
