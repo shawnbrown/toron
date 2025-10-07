1c4b1ef5f38bae41ef01f0ed343d711eb4bd4d83
Shawn Brown <shawnbrown@users.noreply.github.com>
2025-10-07 12:45:38 -0400

    Change `toron.cli` imports and adjust the `scripts` entry point.
    The previous __init__.py was hiding the sub-module names which
    made testing more difficult.

--- src/toron/__main__.py
+++ src/toron/__main__.py
@@ -1,5 +1,5 @@
 """The '-m' entry point for Toron's command-line interface."""
 
 import sys
-from toron.cli import main
+from toron.cli.main import main
 sys.exit(main())
--- src/toron/cli/__init__.py
+++ src/toron/cli/__init__.py
@@ -1,3 +1,1 @@
 """Command-line interface for the Toron project."""
-
-from .main import main
