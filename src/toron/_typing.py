b001cf9d390863ac9c486f3f0bb56d4808eb0f17
Shawn Brown <shawnbrown@users.noreply.github.com>
2025-10-19 04:19:18 -0400

    Reorder `typing` compatibility imports by version number.

--- src/toron/_typing.py
+++ src/toron/_typing.py
@@ -3,13 +3,13 @@ import sys
 from typing import *
 
 
-if sys.version_info < (3, 9):
-    from typing import TextIO  # Not included in `__all__`.
-
-
 if sys.version_info < (3, 8):
     from typing_extensions import Final
     from typing_extensions import Literal
+
+
+if sys.version_info < (3, 9):
+    from typing import TextIO  # Defined but not included in `__all__`.
 
 
 if sys.version_info < (3, 10):
