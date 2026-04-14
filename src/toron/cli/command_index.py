c4bf9ef2caf943c5532d08af49255e91f9355584
Shawn Brown <shawnbrown@users.noreply.github.com>
2026-04-13 23:37:28 -0400

    Reorder functions to match the order of use in `process_index_action()`.

--- src/toron/cli/command_index.py
+++ src/toron/cli/command_index.py
@@ -24,33 +24,6 @@ from ..graph import get_weights
 applogger = logging.getLogger('app-toron')
 
 
-def write_to_stdout(args: argparse.Namespace) -> ExitCode:
-    """Print node index in CSV format to stdout stream."""
-    weights = get_weights(node=args.node, weights=None, header=True)
-    weights_header = next(weights)
-
-    domain_formatted = args.node.domain.get('domain', '')
-    if domain_formatted:
-        domain_formatted = domain_formatted.replace(' ', '_') + '_'
-    header = [f'{domain_formatted}index_code'] + weights_header[1:]
-
-    unique_id_bytes = uuid.UUID(args.node.unique_id).bytes
-    pad_len = len(str(args.node.max_index_id))
-
-    row_count = 0
-    with csv_stdout_writer(args.stdout) as writer:
-        writer.writerow(header)
-
-        for index_id, *row in weights:
-            index_code = index_id_to_code(index_id, unique_id_bytes, pad_len)
-            writer.writerow([index_code] + row)
-            row_count += 1
-
-    applogger.info(f"written {row_count} record{'s' if row_count != 1 else ''}")
-
-    return ExitCode.OK
-
-
 def read_from_stdin(args: argparse.Namespace) -> ExitCode:
     """Insert index records read from stdin stream."""
     reader = csv.reader(args.stdin)
@@ -90,6 +63,33 @@ def read_from_stdin(args: argparse.Namespace) -> ExitCode:
                f'--on-label-conflict and --on-weight-conflict')
         applogger.error(msg)
         return ExitCode.ERR
+
+    return ExitCode.OK
+
+
+def write_to_stdout(args: argparse.Namespace) -> ExitCode:
+    """Print node index in CSV format to stdout stream."""
+    weights = get_weights(node=args.node, weights=None, header=True)
+    weights_header = next(weights)
+
+    domain_formatted = args.node.domain.get('domain', '')
+    if domain_formatted:
+        domain_formatted = domain_formatted.replace(' ', '_') + '_'
+    header = [f'{domain_formatted}index_code'] + weights_header[1:]
+
+    unique_id_bytes = uuid.UUID(args.node.unique_id).bytes
+    pad_len = len(str(args.node.max_index_id))
+
+    row_count = 0
+    with csv_stdout_writer(args.stdout) as writer:
+        writer.writerow(header)
+
+        for index_id, *row in weights:
+            index_code = index_id_to_code(index_id, unique_id_bytes, pad_len)
+            writer.writerow([index_code] + row)
+            row_count += 1
+
+    applogger.info(f"written {row_count} record{'s' if row_count != 1 else ''}")
 
     return ExitCode.OK
 
--- tests/cli/test_command_index.py
+++ tests/cli/test_command_index.py
@@ -7,34 +7,6 @@ from toron import TopoNode
 from toron.cli import command_index
 
 
-class TestIndexWriteToStdout(unittest.TestCase):
-    def test_basic_behavior(self):
-        node = TopoNode()
-        node._connector._unique_id = '11111111-1111-1111-1111-111111111111'
-        node.add_index_columns('state', 'county')
-        node.insert_index([
-            ['state', 'county'],
-            ['Illinois', 'Cook'],
-            ['Indiana', 'Porter'],
-            ['Michigan', 'Cass'],
-        ])
-        dummy_stdout = DummyRedirection()
-        args = argparse.Namespace(command='index', node=node, stdout=dummy_stdout)
-
-        with self.assertLogs('app-toron', level='INFO') as logs_cm:
-            command_index.write_to_stdout(args)  # <- Function under test.
-
-        expected_values = (
-            'index_code,state,county\n'
-            '0X27B3B62D,-,-\n'
-            '1XA0157D6E,Illinois,Cook\n'
-            '2XF38F26EA,Indiana,Porter\n'
-            '3X7429EDA9,Michigan,Cass\n'
-        )
-        self.assertEqual(dummy_stdout.getvalue(), expected_values)
-        self.assertEqual(logs_cm.output, ['INFO:app-toron:written 4 records'])
-
-
 class TestIndexReadFromStdin(unittest.TestCase):
     def test_input_labels_and_weights(self):
         node = TopoNode()
@@ -240,3 +212,31 @@ class TestIndexReadFromStdin(unittest.TestCase):
              'INFO:app-toron.node:loaded 3 index weights',
              'INFO:app-toron.node:replaced 1 index weights'],
         )
+
+
+class TestIndexWriteToStdout(unittest.TestCase):
+    def test_basic_behavior(self):
+        node = TopoNode()
+        node._connector._unique_id = '11111111-1111-1111-1111-111111111111'
+        node.add_index_columns('state', 'county')
+        node.insert_index([
+            ['state', 'county'],
+            ['Illinois', 'Cook'],
+            ['Indiana', 'Porter'],
+            ['Michigan', 'Cass'],
+        ])
+        dummy_stdout = DummyRedirection()
+        args = argparse.Namespace(command='index', node=node, stdout=dummy_stdout)
+
+        with self.assertLogs('app-toron', level='INFO') as logs_cm:
+            command_index.write_to_stdout(args)  # <- Function under test.
+
+        expected_values = (
+            'index_code,state,county\n'
+            '0X27B3B62D,-,-\n'
+            '1XA0157D6E,Illinois,Cook\n'
+            '2XF38F26EA,Indiana,Porter\n'
+            '3X7429EDA9,Michigan,Cass\n'
+        )
+        self.assertEqual(dummy_stdout.getvalue(), expected_values)
+        self.assertEqual(logs_cm.output, ['INFO:app-toron:written 4 records'])
