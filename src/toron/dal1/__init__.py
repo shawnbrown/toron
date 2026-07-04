4f817029345449ca3465e69ae5ee71c5582a2809
Shawn Brown <shawnbrown@users.noreply.github.com>
2026-07-03 21:23:58 -0400

    Raise the minimum required version of SQLite to 3.26.0.
    Older versions of SQLite don't support easy table renaming and
    the DAL1 schema migrations now use `ALTER TABLE X RENAME TO Y`
    (requiring the newer version).

--- src/toron/dal1/__init__.py
+++ src/toron/dal1/__init__.py
@@ -1,10 +1,10 @@
 """Back-end implementation for data access API using SQLite."""
 
-if __import__('sqlite3').sqlite_version_info < (3, 21, 0):
+if __import__('sqlite3').sqlite_version_info < (3, 26, 0):
     raise RuntimeError(
         f"This build of Python {__import__('platform').python_version()} "
         f"is bundled with SQLite {__import__('sqlite3').sqlite_version}. "
-        f"But Toron's \"DAL1\" backend requires SQLite 3.21.0 or newer.\n"
+        f"But Toron's \"DAL1\" backend requires SQLite 3.26.0 or newer.\n"
         f"\n"
         f"Please use an updated Python build with a newer version of SQLite."
     )
