849d3dbc35d18f20f35375c59c133f3af17c364d
Shawn Brown <shawnbrown@users.noreply.github.com>
2026-07-12 22:25:02 -0400

    Add `update_weight()` to the `command_update` module.

--- src/toron/cli/command_update.py
+++ src/toron/cli/command_update.py
@@ -1,12 +1,17 @@
 """Implementation for "update" command."""
 import argparse
 import logging
+from dataclasses import replace
+from .._typing import cast, TYPE_CHECKING
 
 from .common import (
     cli_bind_node,
     process_backup_option,
     ExitCode,
 )
+
+if TYPE_CHECKING:
+    from toron.data_models import WeightGroup
 
 
 applogger = logging.getLogger('app-toron')
@@ -25,5 +30,49 @@ def update_label(args: argparse.Namespace) -> ExitCode:
         applogger.info(f'moved label {args.label!r} to the right')
     else:
         raise Exception
+
+    return ExitCode.OK
+
+
+def update_weight(args: argparse.Namespace) -> ExitCode:
+    """Update index weight group in the given node file."""
+    node = cli_bind_node(args.filepath, mode='rw')
+    process_backup_option(args, node)
+
+    group = node.get_weight_group(args.weight)
+    if not group:
+        applogger.error(f'no weight group named {args.weight!r}')
+        return ExitCode.ERR
+
+    if args.description:
+        node.edit_weight_group(args.weight, description=args.description)
+        applogger.info(f'changed description: {args.description!r}')
+
+    if args.add_selector or args.remove_selector:
+        selectors = list(group.selectors) if group.selectors else []
+        if args.add_selector:
+            selectors.extend(args.add_selector)
+            applogger.info(f"added selectors: "
+                           f"{', '.join(repr(x) for x in args.add_selector)}")
+
+        if args.remove_selector:
+            for sel in args.remove_selector:
+                if sel in selectors:
+                    selectors.remove(sel)
+            applogger.info(f"removed selectors: "
+                           f"{', '.join(repr(x) for x in args.remove_selector)}")
+
+        selectors = sorted(set(selectors))  # Should be unique.
+
+        # Remove any line-breaks in selector text.
+        func = lambda x: x.replace('\n', ' ').replace('\r\n', ' ')
+        selectors = [func(sel) for sel in selectors]
+
+        node.edit_weight_group(args.weight, selectors=selectors)
+
+    if args.make_default:
+        group = cast('WeightGroup', node.get_weight_group(args.weight))
+        node.set_default_weight_group(group)  # Change default WeightGroup.
+        applogger.info(f'set weight {args.weight!r} as the default')
 
     return ExitCode.OK
--- tests/cli/test_command_update.py
+++ tests/cli/test_command_update.py
@@ -3,11 +3,12 @@ import argparse
 import os
 import tempfile
 from .. import _unittest as unittest
-from ..common import TempTopoNodeMixin
+from ..common import ClassTempFileMixin, TempTopoNodeMixin
 from toron import TopoNode, ToronError, bind_node
 
 from toron.cli import command_update
 from toron.cli.common import ExitCode
+from toron.data_models import WeightGroup
 
 
 class TestUpdateLabel(TempTopoNodeMixin, unittest.TestCase):
@@ -69,5 +70,165 @@ class TestUpdateLabel(TempTopoNodeMixin, unittest.TestCase):
                 'with a full traceback even in the CLI because this '
                 'condition should not normally occur and would represent '
                 'a bug that needs fixed, rather than an invalid user input'
+            ),
+        )
+
+
+class TestUpdateWeight(ClassTempFileMixin, unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        node = TopoNode()
+        node.add_weight_group(
+            name='myweight',
+            description='Original description.',
+            selectors=None,
+            is_complete=True,
+            make_default=False,
+        )
+        node.to_file(self.filepath)
+
+    def get_namespace(self, **kwds):  # <- Helper function.
+        """Get parsed arguments as an `argparse.Namespace` instance."""
+        # Start with basic args for "update weight" subcommand.
+        args_dict = {
+            'filepath': self.filepath,
+            'command': 'update',
+            'element': 'weight',
+            'backup': False,
+            'weight': 'myweight',
+            'description': None,
+            'add_selector': None,
+            'remove_selector': None,
+            'make_default': False,
+            'func': command_update.update_weight,
+        }
+        args_dict.update(kwds)
+        return argparse.Namespace(**args_dict)
+
+    def test_description_option(self):
+        args = self.get_namespace(description='New description.')
+
+        exit_code = command_update.update_weight(args)  # Function under test.
+
+        self.assertEqual(exit_code, ExitCode.OK)
+        self.assertEqual(
+            bind_node(self.filepath, mode='ro').weight_groups,
+             [
+                WeightGroup(
+                    id=1,
+                    name='myweight',
+                    description='New description.',
+                    selectors=None,
+                    is_complete=1,
+                ),
+            ],
+        )
+
+    def test_add_and_remove_selector_options(self):
+        # Add selectors when none exist.
+        args = self.get_namespace(add_selector=['[A]', '[C="ccc"]'])
+        exit_code = command_update.update_weight(args)  # Function under test.
+        self.assertEqual(exit_code, ExitCode.OK)
+        self.assertEqual(
+            bind_node(self.filepath, mode='ro').weight_groups,
+             [
+                WeightGroup(
+                    id=1,
+                    name='myweight',
+                    description='Original description.',
+                    selectors=['[A]', '[C="ccc"]'],
+                    is_complete=1,
+                ),
+            ],
+        )
+
+        # Add one more selector to existing selectors.
+        args = self.get_namespace(add_selector=['[B="bbb"][D]'])
+        exit_code = command_update.update_weight(args)  # Function under test.
+        self.assertEqual(exit_code, ExitCode.OK)
+        self.assertEqual(
+            bind_node(self.filepath, mode='ro').weight_groups,
+             [
+                WeightGroup(
+                    id=1,
+                    name='myweight',
+                    description='Original description.',
+                    selectors=['[A]', '[B="bbb"][D]', '[C="ccc"]'],
+                    is_complete=1,
+                ),
+            ],
+        )
+
+        # Remove selector.
+        args = self.get_namespace(remove_selector=['[A]'])
+        exit_code = command_update.update_weight(args)  # Function under test.
+        self.assertEqual(exit_code, ExitCode.OK)
+        self.assertEqual(
+            bind_node(self.filepath, mode='ro').weight_groups,
+             [
+                WeightGroup(
+                    id=1,
+                    name='myweight',
+                    description='Original description.',
+                    selectors=['[B="bbb"][D]', '[C="ccc"]'],
+                    is_complete=1,
+                ),
+            ],
+        )
+
+        # Remove remaining selectors.
+        args = self.get_namespace(remove_selector=['[B="bbb"][D]', '[C="ccc"]'])
+        exit_code = command_update.update_weight(args)  # Function under test.
+        self.assertEqual(exit_code, ExitCode.OK)
+        self.assertEqual(
+            bind_node(self.filepath, mode='ro').weight_groups,
+             [
+                WeightGroup(
+                    id=1,
+                    name='myweight',
+                    description='Original description.',
+                    selectors=None,
+                    is_complete=1,
+                ),
+            ],
+        )
+
+        # Remove selectors when then are none to remove.
+        args = self.get_namespace(remove_selector=['[A]'])
+        exit_code = command_update.update_weight(args)  # Function under test.
+        self.assertEqual(exit_code, ExitCode.OK)
+        self.assertEqual(
+            bind_node(self.filepath, mode='ro').weight_groups,
+            [
+                WeightGroup(
+                    id=1,
+                    name='myweight',
+                    description='Original description.',
+                    selectors=None,
+                    is_complete=1,
+                ),
+            ],
+        )
+
+    def test_make_default_option(self):
+        node = bind_node(self.filepath, mode='ro')
+
+        self.assertIsNone(
+            node.get_default_weight_group(),
+            msg='should start with no default weight group',
+        )
+
+        args = self.get_namespace(make_default=True)
+        exit_code = command_update.update_weight(args)  # Function under test.
+
+        self.assertEqual(exit_code, ExitCode.OK)
+        self.assertEqual(
+            node.get_default_weight_group(),
+            WeightGroup(
+                id=1,
+                name='myweight',
+                description='Original description.',
+                selectors=None,
+                is_complete=1,
             ),
         )
